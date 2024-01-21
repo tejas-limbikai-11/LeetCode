81cd53efd7b585c44178fa28b84dd0307367928f
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-01-21 13:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (45.50%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -14,11 +14,11 @@ class Solution {
         else if(count > 1){
             return false;
         } 
-        else {if(nums[0] >= nums[nums.length -1]){
+        else if(nums[0] >= nums[nums.length -1]){
             return true;
         } 
         else{
             return false;
-        }}
+        }
    }
 }
