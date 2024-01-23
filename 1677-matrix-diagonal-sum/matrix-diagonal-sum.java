f73c46b84a18dc57158bf5ced4189083f8d49da2
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-01-23 18:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (90.56%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -4,10 +4,10 @@ class Solution {
         int sum = 0;
 
         for(int i=0; i<n; i++){
-            //primary diagonal
+            //primary diagonal  (row = column)
             sum += matrix[i][i];
 
-            //secondary diagonal
+            //secondary diagonal (row + column = n-1 i.e. i+j = n-1) 
             int j = n-i-1;
             if(i != j){
                 sum += matrix[i][j];
