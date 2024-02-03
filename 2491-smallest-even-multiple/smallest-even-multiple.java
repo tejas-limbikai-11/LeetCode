ced97dc8430240165cdde0d6ad550c3a6254e49d
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-02-03 13:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (64.82%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,9 +1,7 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
         for(int num=n; num<2*n; num++){
-            if(num%2 == 0 && num%n == 0){
-                return num;
-            }
+            if(num%2 == 0 && num%n == 0) return num;
         }
         return 2*n;
     }
