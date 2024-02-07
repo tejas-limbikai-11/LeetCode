94bb86917817cad99273891aa746308eabb80fc7
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-02-07 16:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (9.43%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,13 +1,38 @@
 class Solution {
     public int countNegatives(int[][] matrix) {
-        int count = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j]<0){
-                    count++;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int res = 0;
+        int lastNeg  = cols - 1;
+
+        for(int row=0; row < rows; row++) {
+            //first element negative => all elements in row are negative
+            if(matrix[row][0] < 0) {
+                res += cols;
+                continue;
+            }
+
+            //last element positive => all elements in row are positive
+            if(matrix[row][cols-1] > 0) {
+                continue;
+            }
+
+            int l = 0;
+            int r = lastNeg;
+            while(l<=r) {
+                int m = l + (r-l)/2;
+                if(matrix[row][m] < 0) {
+                    r = m - 1;
+                }
+                else{
+                    l = m + 1;
                 }
             }
+            //l points to the first negative element,
+            //which means cols - l is a number of such elements
+            res += (cols-l);
+            lastNeg= l;
         }
-        return count;
+        return res;
     }
 }
