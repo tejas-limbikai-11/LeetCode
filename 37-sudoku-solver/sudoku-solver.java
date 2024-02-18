64d1a3dd200f8ef39e8a95ee3b49bdcab11e20d2
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-02-18 19:01:09 +0530

    Time: 11 ms (46.35%) | Memory: 40.9 MB (61.34%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,61 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        sudokuSolver(board, 0, 0);
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, char digit) {
+        //column
+        for(int i=0; i<=8; i++) {
+            if(board[i][col] == digit) {
+                return false;
+            }
+        }
+
+        //row
+        for(int j=0; j<=8; j++) {
+            if(board[row][j] == digit) {
+                return false;
+            }
+        }
+
+        //3x3 grid
+        int sr = (row/3) * 3;
+        int sc = (col/3) * 3;
+        for(int i=sr; i<sr+3; i++) {
+            for(int j=sc; j<sc+3; j++) {
+                if(board[i][j] == digit) {
+                    return false;
+                }
+            }
+        }  
+
+        return true;
+    }
+
+    public boolean sudokuSolver(char[][] board, int row, int col) {
+        if(row == 9) {
+            return true;
+        }
+
+        int nextRow = row, nextCol = col+1;
+        if(col+1 == 9) {
+            nextRow = row+1;
+            nextCol = 0;
+        }
+
+        if(board[row][col] != '.') {
+            return sudokuSolver(board, nextRow, nextCol);
+        }
+
+        for(char digit='1'; digit<='9'; digit++) {
+            if(isSafe(board, row, col, digit)) {
+                board[row][col] = digit;
+                if(sudokuSolver(board, nextRow, nextCol)) {
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+}
