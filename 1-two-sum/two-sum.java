64d7efe3f1ee668dc74c283dccb28408d62f7425
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-02-19 20:30:58 +0530

    Time: 4 ms (66.06%) | Memory: 44.6 MB (78.61%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,40 +1,35 @@
-class Solution {
+public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int copyNums[] = Arrays.copyOf(nums, nums.length);
+        int[] copyNums = Arrays.copyOf(nums, nums.length);
         Arrays.sort(copyNums);
-        int left = 0;
-        int right = copyNums.length-1;
-        int indices[] = new int[2];
-
-        while(left<right) {
+        int left = 0, right = copyNums.length - 1;
+        int[] indices = new int[2];
+        while (left < right) {
             int sum = copyNums[left] + copyNums[right];
-            if(sum == target) {
+            if (sum == target) {
                 indices[0] = left;
                 indices[1] = right;
                 break;
-            } 
-            else if(sum < target) {
+            } else if (sum < target) {
                 left++;
-            } 
-            else{
+            } else {
                 right--;
             }
         }
-
-        for(int i=0; i<nums.length; i++) {
-            if(copyNums[indices[0]] == nums[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (copyNums[indices[0]] == nums[i]) {
                 indices[0] = i;
                 break;
             }
         }
-
-        for(int i=nums.length-1; i>=0; i--) {
-            if(copyNums[indices[1]] == nums[i]) {
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (copyNums[indices[1]] == nums[i]) {
                 indices[1] = i;
                 break;
             }
         }
-
+        
         return indices;
     }
 }
