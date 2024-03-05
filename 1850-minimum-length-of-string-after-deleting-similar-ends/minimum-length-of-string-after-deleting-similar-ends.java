8f164b79917d5ffa61382f9cee764253c835e18a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-06 00:51:34 +0530

    Time: 4 ms (80.63%) | Memory: 45.2 MB (40.95%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -6,22 +6,14 @@ class Solution {
         int left = 0;
         int right = s.length()-1;
 
-        while(left<right) {
-            if(s.charAt(left) != s.charAt(right)) {
-                break;
-            }
+        while(left<right && s.charAt(left) == s.charAt(right)) {
+            char ch = s.charAt(left);
 
-            if(s.charAt(left) == s.charAt(right)) {
-                if(s.charAt(left+1) == s.charAt(left) && right-left>1) {
-                    left++;
-                } 
-                else if(s.charAt(right-1) == s.charAt(right)  && right-left>1) {
-                    right--;
-                }
-                else{
-                    left++;
-                    right--;
-                }
+            while(left<= right && ch == s.charAt(left)) {
+                left++;
+            }
+            while(left<= right && ch == s.charAt(right)) {
+                right--;
             }
         }
 
