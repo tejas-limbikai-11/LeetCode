c1e0cffa8a998a11586e4fad6b16d7edf2e8e3b5
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-07 20:54:44 +0530

    Time: 1 ms (23.97%) | Memory: 44.5 MB (13.37%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        inorder(root, result);
+
+        for(int i=0; i<result.size()-1; i++) {
+            if(result.get(i) >= result.get(i+1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void inorder(TreeNode root, ArrayList<Integer> list) {
+        if(root == null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
