4c0bf305ec65dab076077a63948e9eb66021880a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-16 20:46:30 +0530

    Time: 2 ms (73.93%) | Memory: 54.9 MB (19.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int product = 1;
-        int count = 0;
-        int arr[] = new int[nums.length];
+        int leftProduct[] = new int[nums.length];
+        int rightProduct[] = new int[nums.length];
+        int ans[] = new int[nums.length];
+
         for(int i=0; i<nums.length; i++) {
-            if(nums[i] == 0) {
-                count++;
-            }
-            else product *= nums[i];
+            product *= nums[i];
+            leftProduct[i] = product;
         }
 
-        if(count > 1) {
-            arr = new int[nums.length];
-            return arr;   
+        product = 1;
+
+        for(int i=nums.length-1; i>=0; i--) {
+            product *= nums[i];
+            rightProduct[i] = product;
         }
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == 0 && count == 1) {
-                arr = new int[nums.length];
-                arr[i] = product;
-                return arr;
-            }
-            else arr[i] = product / nums[i];
+
+        ans[0] =  rightProduct[1];
+        ans[nums.length-1] = leftProduct[nums.length-2];
+
+        for(int i=1; i<nums.length-1; i++) {
+            ans[i] = leftProduct[i-1] * rightProduct[i+1];
         }
-        return arr;
+
+        return ans;
     }
 }
