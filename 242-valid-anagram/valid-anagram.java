879d2fe86035b7767490386608f9777d8217459f
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-27 19:59:17 +0530

    Time: 20 ms (13.45%) | Memory: 44.5 MB (43.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,27 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int sFrequency[] = new int[26];
-        int tFrequency[] = new int[26];
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        // Frequency of characters in String s
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            sFrequency[ch - 'a']++;
+        for(int i=0; i<s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1) ;
         }
 
-        // Frequency of characters in String t
-        for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            tFrequency[ch - 'a']++;
-        }
-
-        //comparison
-        for(int i=0; i<26; i++){
-            if(sFrequency[i] != tFrequency[i]){
-                return false;
+        for(int i=0; i<t.length(); i++) {
+            if(map.get(t.charAt(i)) != null) {
+                if(map.get(t.charAt(i)) == 1) map.remove(t.charAt(i));
+                else map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
             }
+            else return false;
         }
-
-        return true;
+        return map.isEmpty();
     }
 }
