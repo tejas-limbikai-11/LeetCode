91e2e700832f9fd0becb62a59de759249e2a03ad
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-31 01:10:24 +0530

    Time: 45 ms (53.28%) | Memory: 47.8 MB (11.97%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,32 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int lNear = 0;
-        int lFar = 0;
-        int ans = 0;
+        return slidingWindow(nums, k) - slidingWindow(nums, k-1);
+    }
 
-        Map<Integer, Integer> map = new HashMap<>();
+    public int slidingWindow(int[] nums, int k) {
+        int left = 0, right = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int r = 0; r < nums.length; r++) {
-            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+        while(right < nums.length) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
 
             while(map.size() > k) {
-                map.put(nums[lNear], map.get(nums[lNear]) - 1);
-                if(map.get(nums[lNear]) == 0) {
-                    map.remove(nums[lNear]);
-                }
-                lNear++;
-                lFar = lNear;
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if(map.get(nums[left]) == 0) map.remove(nums[left]);
+                left++;
             }
-
-            while(map.containsKey(nums[lNear]) && map.get(nums[lNear]) > 1) {
-                map.put(nums[lNear], map.get(nums[lNear]) - 1);
-                lNear++;
-            }
-
-            if(map.size() == k) {
-                ans += lNear - lFar + 1;
-            }
+            count += right - left + 1;
+            right++;
         }
-        return ans;
+        return count;
     }
 }
