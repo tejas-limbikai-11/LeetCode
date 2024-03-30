08271b4a0cbf3564a01e9cc74cacb31fa7d00437
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-30 20:50:16 +0530

    Time: 8 ms (12.46%) | Memory: 41.9 MB (22.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,13 +1,63 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
-            }  
-        }      
+    static class Node {
+        Node children[] = new Node[26];
+        int freq = 0;
+        boolean isEndOfWord = false;
+
+        public Node() {
+            for(int i=0; i<children.length; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public Node root = new Node();
+
+    public void insert(String word) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+            curr.freq++;
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public String longestCommonPrefixFunction() {
+        Node curr = root;
+        String prefix = "";
+        while(curr != null) {
+            int singleChildIdx = -1;
+            for(int i=0; i<26; i++) {
+                if(curr.children[i] != null) {
+                    if(singleChildIdx == -1) {
+                        singleChildIdx = i;
+                    } 
+                    else return prefix;
+                }
+            }
+            if(singleChildIdx != -1) {
+                prefix += (char) (singleChildIdx + 'a');
+                curr = curr.children[singleChildIdx];
+                if(curr.isEndOfWord) return prefix;
+            } 
+            else break;
+        }
         return prefix;
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        for(String str : strs) {
+            if(str.equals("")) return "";
+        }
+
+        root = new Node();
+        for(int i=0; i<strs.length; i++) {
+            insert(strs[i]);
+        }
+        return longestCommonPrefixFunction();
     }
 }
