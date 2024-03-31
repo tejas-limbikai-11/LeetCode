6ebb7f3c39b729cd2b06feb4ac3afee5ccd28d35
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-03-31 23:15:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.67%) - LeetSync

--- 3371-harshad-number/harshad-number.java
+++ 3371-harshad-number/harshad-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
         int sumOfDigits = 0;
-        if(x < 10) sumOfDigits = x;
-        else if(x == 100) sumOfDigits = 1;
-        else sumOfDigits = x / 10 + x % 10;
-
-        if(x % sumOfDigits == 0) return sumOfDigits;
+        int num = x;
+        while(x > 0) {
+            sumOfDigits += x % 10;
+            x /= 10;
+        }
+        if(num % sumOfDigits == 0) return sumOfDigits;
         else return -1;
     }
 }
