4d7b3dfe9b2af9c4b4280ca9b6e317111b553b34
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-02 00:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (37.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        //fibonacci
-        int a = 0;
-        int b = 1;
-
-        int ways = 0;
-
-        for(int i=0; i<n; i++){
-            ways = a + b;
-            a = b;
-            b = ways;
-        }
-        return ways;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return countWays(n, dp);
     }
+     public static int countWays(int n, int dp[]) {
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+        dp[n] = countWays(n-1, dp) + countWays(n-2, dp);
+        return dp[n];
+    }    
+
 }
