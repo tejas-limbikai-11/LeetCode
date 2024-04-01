871b9d59379c5615b9eeb5f5619bb4bf62eb688a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-02 00:49:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (64.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,33 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return countWays(n, dp);
+        // int dp[] = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return countWays(n, dp);
+
+        return countWays(n);
     }
-     public static int countWays(int n, int dp[]) {
+
+    //memoization
+/*
+    public static int countWays(int n, int dp[]) {
         if(n == 0) return 1;
         if(n < 0) return 0;
 
         if(dp[n] != -1) return dp[n];
         dp[n] = countWays(n-1, dp) + countWays(n-2, dp);
         return dp[n];
-    }    
+    }   
+*/
+    //tabulation
+    public static int countWays(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        for(int i=1; i<=n; i++) {
+            if(i == 1) dp[i] = dp[i-1];
+            else dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }  
 
 }
