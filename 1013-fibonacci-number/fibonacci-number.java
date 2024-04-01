f4fb52be960d10048be2982cfe7b71bc16b87783
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-01 23:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (75.79%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        int fnmin1 = fib(n-1);
-        int fnmin2 = fib(n-2);
+        int f[] = new int[n+1];
+        return fibonacci(n, f);
+    }
 
-        return fnmin1 + fnmin2;
+    public int fibonacci(int n, int f[]) {
+        if(n == 0 || n == 1) return n;
+
+        if(f[n] != 0) return f[n];
+        f[n] = fibonacci(n-1, f) + fibonacci(n-2, f);
+        return f[n];
     }
 }
