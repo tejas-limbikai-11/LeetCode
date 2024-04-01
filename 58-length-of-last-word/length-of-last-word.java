da59688b434b5818ea6f2178dab62158698e7cdb
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-01 13:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (30.67%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -2,10 +2,10 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int length = 0;
         for(int i=s.length()-1; i>=0; i--) {
-            if(s.charAt(i) > 64 && s.charAt(i) < 123) {
+            if(s.charAt(i) != ' ') {
                 length++;
             }
-            if(Character.isWhitespace(s.charAt(i)) && length > 0) {
+            else if(length > 0){
                 break;
             }
         }
