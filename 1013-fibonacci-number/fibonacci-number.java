8e24c8921b293aa8a3d12e90a3c99a615ff60856
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-01 23:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (75.79%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,28 @@
 class Solution {
     public int fib(int n) {
-        int f[] = new int[n+1];
-        return fibonacci(n, f);
+        // int f[] = new int[n+1];
+        // return fibMemoization(n, f);
+
+        return fibTabulation(n);
     }
 
-    public int fibonacci(int n, int f[]) {
+/*
+    public static int fibMemoization(int n, int f[]) {         //O(n)
         if(n == 0 || n == 1) return n;
-
         if(f[n] != 0) return f[n];
-        f[n] = fibonacci(n-1, f) + fibonacci(n-2, f);
+        f[n] = fibMemoization(n-1, f) + fibMemoization(n-2, f);
         return f[n];
+    }  
+*/ 
+
+    public static int fibTabulation(int n) {
+        if(n == 0) return 0;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
