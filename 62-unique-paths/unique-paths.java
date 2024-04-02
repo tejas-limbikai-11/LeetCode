8f93d2c3e3be908367139486f7930c44d70c37bb
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-03 00:07:04 +0530

    Time: 1 ms (11.31%) | Memory: 40 MB (91.77%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -5,28 +5,56 @@ class Solution {
             Arrays.fill(d, -1);
         }
 
-        return uniquePathsMemoization(0, 0, n, m, dp);
+        return uniquePathsTabulation(0, 0, n, m, dp);
     }
 
-    public static int uniquePathsMemoization(int i, int j, int n, int m, int dp[][]) {
+    public static int uniquePathsTabulation(int i, int j, int n, int m, int dp[][]) {
         if(i == n-1 && j == m-1) return dp[i][j] = 1;
+        for(i=n-1; i>=0; i--) {
+            for(j=m-1; j>=0; j--) {
 
-        int downPaths = 0;
-        int rightPaths = 0;
+                if(i == n-1 && j == m-1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
 
-        if(dp[i][j] != -1) {
-            return dp[i][j];
+                int downPaths = 0;
+                int rightPaths = 0;
+
+                if(i+1 < n) {
+                    downPaths = dp[i+1][j];
+                }
+
+                if(j+1 < m) {
+                    rightPaths = dp[i][j+1];
+                }
+
+                int totalPaths = rightPaths + downPaths;
+                dp[i][j] = totalPaths;
+            }
         }
-
-        if(i+1 < n) {
-            downPaths = uniquePathsMemoization(i+1, j, n, m, dp);
-        }
-
-        if(j+1 < m) {
-            rightPaths = uniquePathsMemoization(i, j+1, n, m, dp);
-        }
-
-        int totalPaths = rightPaths + downPaths;
-        return dp[i][j] = totalPaths;
+        return dp[0][0];
     }
+
+    // public static int uniquePathsMemoization(int i, int j, int n, int m, int dp[][]) {
+    //     if(i == n-1 && j == m-1) return dp[i][j] = 1;
+
+    //     int downPaths = 0;
+    //     int rightPaths = 0;
+
+    //     if(dp[i][j] != -1) {
+    //         return dp[i][j];
+    //     }
+
+    //     if(i+1 < n) {
+    //         downPaths = uniquePathsMemoization(i+1, j, n, m, dp);
+    //     }
+
+    //     if(j+1 < m) {
+    //         rightPaths = uniquePathsMemoization(i, j+1, n, m, dp);
+    //     }
+
+    //     int totalPaths = rightPaths + downPaths;
+    //     return dp[i][j] = totalPaths;
+    // }
 }
