3f098f6af9cb33276a3254552508ddf5c7497797
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-03 19:04:39 +0530

    Time: 208 ms (25.72%) | Memory: 41.4 MB (61.18%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -7,9 +7,11 @@ class Solution {
         m = board.length;
         n = board[0].length;
 
+        boolean[][] visited = new boolean[m][n];
+
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(board[i][j] == word.charAt(0) && backtrack(board, i, j, 0, word)) {
+                if(board[i][j] == word.charAt(0) && backtrack(board, i, j, 0, word, visited)) {
                     return true;
                 }
             }
@@ -17,27 +19,27 @@ class Solution {
         return false;
     }
 
-    public boolean backtrack(char[][] board, int i, int j, int idx, String word) {
+    public boolean backtrack(char[][] board, int i, int j, int idx, String word, boolean[][] visited) {
         if(idx == word.length()) return true;
 
-        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '$') {
+        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j]) {
             return false;
         }
 
         if(board[i][j] != word.charAt(idx)) return false;
 
-        char temp = board[i][j];
-        board[i][j] = '$';
+        visited[i][j] = true;
 
         for(int dir[]: directions) {
             int i_next = i + dir[0];
             int j_next = j + dir[1];
 
-            if(backtrack(board, i_next, j_next, idx + 1, word)) {
+            if(backtrack(board, i_next, j_next, idx + 1, word, visited)) {
                 return true;
             }
         }
-        board[i][j] = temp;
+
+        visited[i][j] = false;
         return false;
     }
 }
