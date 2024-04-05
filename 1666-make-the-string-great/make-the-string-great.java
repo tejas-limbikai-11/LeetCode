cd7d294b36d7e8d3b914483edbb78580b355c664
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-05 20:30:53 +0530

    Time: 2 ms (88.33%) | Memory: 42 MB (67.44%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,16 +1,17 @@
 class Solution {
     public String makeGood(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        boolean changed = true;
-        while(changed) {
-            changed = false;
-            for(int i=0; i<sb.length()-1; i++) {
-                if(Math.abs(sb.charAt(i) - sb.charAt(i+1)) == 32) {
-                    sb.delete(i, i + 2);
-                    changed = true;
-                    break;
-                }
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack();
+
+        for(char c: s.toCharArray()) {
+            if(!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop();
             }
+            else stack.push(c);
+        }
+
+        for(char c: stack) {
+            sb.append(c);
         }
         return sb.toString();
     }
