d2af85e3226cd53b25df06ef3811f687915eb004
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-07 22:48:33 +0530

    Time: 3 ms (5.68%) | Memory: 44.3 MB (5.28%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,26 +1,33 @@
 class Solution {
+    int[][] dp = new int[101][101];
+
+    public boolean solve(int idx, int open, String s, int n) {
+        if(idx == n) return open == 0;
+
+        if(dp[idx][open] != -1) return dp[idx][open] == 1;
+
+        boolean isValid = false;
+
+        if(s.charAt(idx) == '*') {
+            isValid |= solve(idx+1, open+1, s, n);
+            isValid |= solve(idx+1, open, s, n);
+            if(open > 0) {
+                isValid |= solve(idx+1, open-1, s, n);
+            }
+        } else if(s.charAt(idx) == '(') {
+            isValid |= solve(idx+1, open+1, s, n);
+        } else if(open > 0) {
+            isValid |= solve(idx+1, open-1, s, n);
+        }
+        dp[idx][open] = isValid ? 1 : 0;
+        return isValid;
+    }
+
     public boolean checkValidString(String s) {
-        Stack<Integer> openSt = new Stack<>();
-        Stack<Integer> asteriskSt = new Stack<>();
-
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(') openSt.push(i);
-            else if(s.charAt(i) == '*') asteriskSt.push(i);
-            else {
-                if(!openSt.isEmpty()) openSt.pop();
-                else if(!asteriskSt.isEmpty()) asteriskSt.pop();
-                else return false;
-            }
+        int n = s.length();
+        for(int[] d: dp) {
+            Arrays.fill(d, -1);
         }
-
-        while(!openSt.isEmpty() && !asteriskSt.isEmpty()) {
-            if(openSt.peek() > asteriskSt.peek()) {
-                return false;
-            }
-            openSt.pop();
-            asteriskSt.pop();
-        }
-
-        return openSt.isEmpty();
+        return solve(0, 0, s, n);
     }
 }
