f6915e78006de03adbaa407500c1848aac1b3af7
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-09 00:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (84.11%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -18,28 +18,3 @@ class Solution {
         return p2 == 0;
     }
 }
-
-// class Solution {
-//     public boolean checkValidString(String s) {
-//         int p1=0,p2=0;
-//         for(int i=0;i<s.length();i++){
-//             if(s.charAt(i)=='('){
-//                 p1++;
-//                 p2++;
-//             } else if(s.charAt(i)==')'){
-//                 p1--;
-//                 p2--;
-//             } else if(s.charAt(i)=='*'){
-//                 p1++;
-//                 p2--;
-//             }
-//             if(p1<0){
-//                 return false;
-//             }
-//             if(p2<0){
-//                 p2 = 0;
-//             }
-//         }
-//         return p2==0;
-//     }
-// }
