329253716c651f33a904bb17c2d45083223d4115
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-15 20:30:08 +0530

    Time: 19 ms (86.96%) | Memory: 50.4 MB (85.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,24 +1,29 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int dp[][] = new int[n+1][m+1];
-        for(int d[]: dp) {
-            Arrays.fill(d, -1);
-        }
-        return lcsMemoization(text1, text2, n, m, dp);
+        return lcsTabulation(text1, text2);
     }
 
-    public static int lcsMemoization(String str1, String str2, int n, int m, int dp[][]) {
-        if(n == 0 || m == 0) return 0;
-        if(dp[n][m] != -1) return dp[n][m];
-        if(str1.charAt(n-1) == str2.charAt(m-1)) {
-            return dp[n][m] = lcsMemoization(str1, str2, n-1, m-1, dp) + 1;
-        } 
-        else {
-            int ans1 = lcsMemoization(str1, str2, n-1, m, dp);
-            int ans2 = lcsMemoization(str1, str2, n, m-1, dp);
-            return dp[n][m] = Math.max(ans1, ans2);
+    public int lcsTabulation(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<m+1; j++) {
+                if(i == 0 || j == 0) dp[i][j] = 0;
+            }
         }
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<m+1; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } 
+                else {                                        
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
