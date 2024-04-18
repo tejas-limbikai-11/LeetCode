cf4dd709356d76aa91540c946f1765554cbe4279
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-18 22:27:24 +0530

    Time: 9 ms (14.23%) | Memory: 46 MB (22.25%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,5 +1,5 @@
 class Solution {
-    int perimeter = 0;
+    private static final int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public int islandPerimeter(int[][] grid) {
         int m = grid.length;
@@ -8,28 +8,37 @@ class Solution {
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    return perimeter;
+                    return bfs(grid, i, j);
                 }
             }
         }
         return -1;
     }
 
-    public void dfs(int grid[][], int i, int j) {
+    public int bfs(int grid[][], int i, int j) {
         int m = grid.length;
         int n = grid[0].length;
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == 0) {
-            perimeter++;
-            return;
-        }
-        if(grid[i][j] == -1) return;
-
+        int perimeter = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {i, j});
         grid[i][j] = -1;
 
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        while(!q.isEmpty()) {
+            int[] it = q.poll();
+            for(int dir[]: directions) {
+                int a = it[0] + dir[0];
+                int b = it[1] + dir[1];
+
+                if(a < 0 || a >= m || b < 0 || b >= n || grid[a][b] == 0) {
+                    perimeter++;
+                }
+                else if(grid[a][b] == -1) continue;
+                else {
+                    q.offer(new int[] {a, b});
+                    grid[a][b] = -1;
+                }
+            }
+        }
+        return perimeter;
     }
 }
