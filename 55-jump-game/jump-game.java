eead49692380cd4e0af3526e87b4579ce816deb1
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-24 01:13:53 +0530

    Time: 3 ms (55.68%) | Memory: 46.1 MB (5.21%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        dp[n-1] = 0;
-
-        for(int i=n-2; i>=0; i--) {
-            int steps = nums[i];
-            int jumps = Integer.MAX_VALUE;
-            for(int j=i+1; j<=(i+steps) && j<n; j++) {
-                if(dp[j] != -1) jumps = Math.min(jumps, dp[j] + 1);
-            }
-            if(jumps != Integer.MAX_VALUE) dp[i] = jumps;
+        int reachable = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(i > reachable) return false;
+            reachable = Math.max(reachable, i + nums[i]);
         }
-        return dp[0] != -1;
+        return true;
     }
 }
