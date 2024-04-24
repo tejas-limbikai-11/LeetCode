b99ed99b9d832e34e5e72aae4b5646f9cfb41a20
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-24 18:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (95.77%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,20 +1,19 @@
 class Solution {
     public int tribonacci(int n) {
-        return trib(n);
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return trib(n, dp);
     }
 
-    public int trib(int n) {
+    public int trib(int n, int[] dp) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        int dp[] = new int[n+1];
+        if(dp[n] != -1) return dp[n];
 
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int tnmin1 = trib(n-1, dp);
+        int tnmin2 = trib(n-2, dp);
+        int tnmin3 = trib(n-3, dp);
 
-        for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        }
-        return dp[n];
+        return dp[n] = tnmin1 + tnmin2 + tnmin3;
     }
 }
