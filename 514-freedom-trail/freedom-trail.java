10c31287401d89e0e6d558bc00593cabc2e538ad
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-04-27 17:11:48 +0530

    Time: 219 ms (5.83%) | Memory: 45.3 MB (27.18%) - LeetSync

--- 514-freedom-trail/freedom-trail.java
+++ 514-freedom-trail/freedom-trail.java
@@ -1,28 +1,34 @@
 class Solution {
     public int findRotateSteps(String ring, String key) {
+        int n = ring.length();
+        int m = key.length();
+
         int[][] dp = new int[101][101];
-        for(int d[]: dp) Arrays.fill(d, -1);
-        return solve(0, 0, ring, key, dp);
+        for(int d[]: dp) Arrays.fill(d, Integer.MAX_VALUE);
+        
+        for(int ringIdx = 0; ringIdx < n; ringIdx++) {
+            dp[ringIdx][m] = 0;
+        }
+
+        for(int keyIdx = m-1; keyIdx >= 0; keyIdx--) {
+            for(int ringIdx = 0; ringIdx < n; ringIdx++) {
+                int result = Integer.MAX_VALUE;
+                for(int i=0; i<ring.length(); i++) {
+                    if(ring.charAt(i) == key.charAt(keyIdx)) {
+                        int steps = countSteps(ringIdx, i, ring.length());
+                        int totalSteps = 1 + steps + dp[i][keyIdx + 1]; 
+                        result = Math.min(result, totalSteps);
+                    }
+                }
+                dp[ringIdx][keyIdx] = result;
+            } 
+        }
+        return dp[0][0];
     }
 
     public int countSteps(int ringIdx, int i, int n) {
         int dist = Math.abs(i - ringIdx);
         int wrapAround = n - dist;
         return Math.min(dist, wrapAround);
-    }
-
-    public int solve(int ringIdx, int keyIdx, String ring, String key, int[][] dp) {
-        if(keyIdx == key.length()) return 0;
-        int result = Integer.MAX_VALUE;
-        if(dp[ringIdx][keyIdx] != -1) return dp[ringIdx][keyIdx];
-
-        for(int i=0; i<ring.length(); i++) {
-            if(ring.charAt(i) == key.charAt(keyIdx)) {
-                int steps = countSteps(ringIdx, i, ring.length());
-                int totalSteps = 1 + steps + solve(i, keyIdx + 1, ring, key, dp); 
-                result = Math.min(result, totalSteps);
-            }
-        }
-        return dp[ringIdx][keyIdx] = result;
     }
 }
