04067702b0475510dfa845964c2ca2883f6fe22d
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-05-10 12:09:20 +0530

    Time: 24 ms (43.78%) | Memory: 75 MB (10.44%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -1,12 +1,30 @@
 class Solution {
+    private ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;    
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
     public ListNode removeNodes(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode nextNode = removeNodes(head.next);
-        if(head.val < nextNode.val) {
-            head = null;
-            return nextNode;
+        head = reverseList(head);
+        ListNode prev = null;
+        ListNode curr = head;
+        int max = 0;
+
+        while(curr != null) {
+            max = Math.max(max, curr.val);
+            if(curr.val < max) {
+                prev.next = curr.next;
+                ListNode deleted = curr;
+                curr = curr.next;
+                deleted.next = null;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
         }
-        head.next = nextNode;
-        return head;
+        return reverseList(head);
     }
 }
