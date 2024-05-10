a9a83a3a4f36be04ef472a29eb7bd8ab7249b51a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-05-11 00:30:30 +0530

    Time: 250 ms (65.94%) | Memory: 44.9 MB (72.05%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+        int n = arr.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a[0]));
 
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                double div = (double) arr[i] / arr[j];
-                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
-                if(pq.size() > k) pq.poll();
-            }
+        for(int i=0; i<n; i++) {
+            pq.offer(new double[]{1.0 * arr[i] / arr[n-1], (double) i, (double) (n-1)});
         }
+
+        int smallest = 1;
+        while(smallest < k) {
+            double kth[] = pq.poll();
+            int i = (int) kth[1];
+            int j = (int) kth[2] - 1;
+            pq.offer(new double[]{1.0 * arr[i] / arr[j], (double) i, (double) j});
+            smallest++;
+        }
+
         double kth[] = pq.peek();
-        int result[] = new int[2];
-        result[0] = (int) kth[1];
-        result[1] = (int) kth[2];
-        return result;
+        int i = (int) kth[1];
+        int j = (int) kth[2];
+        return new int[] {arr[i], arr[j]};
     }
 }
