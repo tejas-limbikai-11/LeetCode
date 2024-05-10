3a44bfd282a1353fbbc59debb26509a98e11a708
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-05-11 00:11:01 +0530

    Time: 361 ms (57.64%) | Memory: 71.8 MB (39.30%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,41 +1,18 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    static class Fraction implements Comparable<Fraction>{
-        double f;
-        int i;
-        int j;
-          
-        public Fraction(double f, int i, int j) {
-            this.f = f;
-            this.i = i;
-            this.j = j;
-        }
-        @Override
-        public int compareTo(Fraction p2) {
-            return Double.compare(this.f, p2.f);
-        }
-    }
-
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        int ans[] = new int[2];
-        PriorityQueue<Fraction> pq = new PriorityQueue<>();
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) { 
-                pq.add(new Fraction(((double)arr[i] / arr[j]), arr[i], arr[j]));
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+                if(pq.size() > k) pq.poll();
             }
         }
-
-        int count = 0;
-        while (!pq.isEmpty()) {
-            Fraction curr = pq.poll();
-            if (++count == k) {
-                ans[0] = curr.i;
-                ans[1] = curr.j;
-                return ans;
-            }
-        }
-        return ans;
+        double kth[] = pq.peek();
+        int result[] = new int[2];
+        result[0] = (int) kth[1];
+        result[1] = (int) kth[2];
+        return result;
     }
 }
