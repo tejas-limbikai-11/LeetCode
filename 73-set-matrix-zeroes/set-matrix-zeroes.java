f425376f61d189003edecd9183a1a26c3368d2e2
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-07 14:44:41 +0530

    Time: 1 ms (76.72%) | Memory: 45.3 MB (72.07%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,23 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        boolean vis[][] = new boolean[n][m];
-
+        int col0 = 1;
         for(int i=0; i<matrix.length; i++) {
             for(int j=0; j<matrix[0].length; j++) {
                 if(matrix[i][j] == 0) {
-                    for(int k=0; k<matrix.length; k++) vis[k][j] = true;
-                    for(int k=0; k<matrix[0].length; k++) vis[i][k] = true;
+                    matrix[i][0] = 0;
+
+                    if(j != 0) matrix[0][j] = 0;
+                    else col0 = 0;
                 }
             }
-       }
-       for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(vis[i][j] == true) {
-                    matrix[i][j] = 0;
+        }
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[0].length; j++) {
+                if(matrix[i][j] != 0) {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
-       }
+        }
+        if(matrix[0][0] == 0) {
+            for(int j=1; j<matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0) {
+            for(int i=0; i<matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
