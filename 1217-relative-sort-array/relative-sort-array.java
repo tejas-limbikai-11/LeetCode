ecd28d6ebcfb81f57d486ea48d1d1b42284f6a14
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-11 11:15:51 +0530

    Time: 4 ms (23.80%) | Memory: 41.9 MB (61.60%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,29 +1,36 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num: arr1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int i=0; i<arr2.length; i++) {
+            map.put(arr2[i], i);
         }
-
-        List<Integer> result = new ArrayList<>();
-        for(int num: arr2) {
-            if(map.containsKey(num)) {
-                for(int i=0; i<map.get(num); i++) result.add(num);
-                map.remove(num);
+        
+        int largeValue = 1000000000;
+        for(int i=0; i<arr1.length; i++) {
+            if(!map.containsKey(arr1[i])) {
+                map.put(arr1[i], largeValue);
             }
         }
 
-        List<Integer> remaining = new ArrayList<>();
-        for(int num: map.keySet()) {
-            for(int i=0; i<map.get(num); i++) remaining.add(num);
-        }
-        Collections.sort(remaining);
-        result.addAll(remaining);
+        Comparator<Integer> comparator = (num1, num2) -> {
+            int index1 = map.get(num1);
+            int index2 = map.get(num2);
+            if(index1 == index2) {
+                return Integer.compare(num1, num2);
+            }
+            return Integer.compare(index1, index2);
+        };
 
-        int resultArray[] = new int[result.size()];
-        for(int i=0; i<result.size(); i++) {
-            resultArray[i] = result.get(i);
+        List<Integer> list1 = new ArrayList<>();
+        for(int num: arr1) {
+            list1.add(num);
         }
-        return resultArray;
-    }
+        Collections.sort(list1, comparator);
+
+        int sortedArray[] = new int[list1.size()];
+        for(int i=0; i<list1.size(); i++) {
+            sortedArray[i] = list1.get(i);
+        }
+        return sortedArray;
+   }
 }
