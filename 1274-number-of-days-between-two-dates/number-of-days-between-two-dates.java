4f90ed2b10ae772ba33373cf355f16cdd7e42b47
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-14 23:04:01 +0530

    Time: 2 ms (47.37%) | Memory: 42 MB (12.38%) - LeetSync

--- 1274-number-of-days-between-two-dates/number-of-days-between-two-dates.java
+++ 1274-number-of-days-between-two-dates/number-of-days-between-two-dates.java
@@ -1,8 +1,27 @@
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-
 class Solution {
+    public int[] months = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public int daysBetweenDates(String date1, String date2) {
-        return Math.abs((int) ChronoUnit.DAYS.between(LocalDate.parse(date1), LocalDate.parse(date2)));
+        return Math.abs(from1971(date1) - from1971(date2));
+    }
+
+    public int from1971(String date) {
+        String parts[] = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+
+        int count = day;
+        for(int i=1971; i<year; i++) {
+            count += (isLeapYear(i)) ? 366: 365;
+        }
+        for(int i=0; i<month-1; i++) {
+            count += months[i];
+        }
+        if(month > 2 && isLeapYear(year)) count += 1;
+        return count;
+    }
+
+    public boolean isLeapYear(Integer year) {
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
     }
 }
