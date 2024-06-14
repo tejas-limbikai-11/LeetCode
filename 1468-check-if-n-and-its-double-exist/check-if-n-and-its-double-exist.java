73abe448d83635c74b7418f1093a13ec18805f3c
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-14 23:48:09 +0530

    Time: 3 ms (35.39%) | Memory: 43.8 MB (7.85%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,8 +1,12 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr.length; j++) {
-                if(i != j && arr[i] == 2 * arr[j]) return true;
+            map.put(arr[i], i);
+        }
+        for(int i=0; i<arr.length; i++) {
+            if(map.containsKey(2 * arr[i]) && map.get(2 * arr[i]) != i) {
+                return true;
             }
         }
         return false;
