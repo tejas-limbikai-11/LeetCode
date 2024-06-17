8bcbf010e825cc3026793ccebd6a7c5e3f99df60
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-17 21:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (56.76%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,6 +1,13 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int temp = (int) Math.sqrt(num);
-        return temp * temp == num;
+        long left = 1, right = num;
+
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            if(mid * mid == num) return true;
+            else if(mid * mid < num) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
     }
 }
