d1c9454ccde7a148ee515527987056ef582b4a44
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-18 18:35:45 +0530

    Time: 28 ms (33.91%) | Memory: 45.8 MB (39.31%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -2,16 +2,32 @@ class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         int n = difficulty.length;
         int m = worker.length;
-        int maxProfit = 0;
 
-        for(int i=0; i<m; i++) {
-            int currProfit = 0;
-            for(int j=0; j<n; j++) {
-                if(difficulty[j] <= worker[i]) {
-                    currProfit = Math.max(currProfit, profit[j]);
-                }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        for(int i=0; i<n; i++) {
+            int prof = profit[i];
+            int diff = difficulty[i];
+            pq.offer(new int[]{prof, diff});
+        }
+
+        //sort worker array in descending order
+        Arrays.sort(worker);
+        for(int i=0; i<m/2; i++) {
+            int temp = worker[i];
+            worker[i] = worker[m-1-i];
+            worker[m-1-i] = temp;
+        }
+
+        int i = 0;
+        int maxProfit = 0;
+        while(i < m && !pq.isEmpty()) {
+            if(pq.peek()[1] > worker[i]) {
+                pq.poll();
+            } else {
+                maxProfit += pq.peek()[0];
+                i++;
             }
-            maxProfit += currProfit;
         }
         return maxProfit;
     }
