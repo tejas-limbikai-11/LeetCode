09722aba4bb1e52977d7560c0bcd07818ce841fe
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-06-25 23:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (52.04%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,36 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Stack<TreeNode> s = new Stack<>();
-            if(root == null) return list;
-            TreeNode node = root;
-            while(true) {
-                if(node != null) {
-                    s.push(node);
-                    node = node.left;
+        List<Integer> list = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left == null) {
+                list.add(curr.val);
+                curr = curr.right;
+            }
+            else {
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                if(prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
                 }
                 else {
-                    if(s.isEmpty()) break;
-                    node = s.pop();
-                    list.add(node.val);
-                    node = node.right;
+                    prev.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
                 }
             }
-        return list;
+        }
+        return list;        
     }
 }
