4a3807eb8956521ed55841a1535cb2f080fc2c16
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-01 11:33:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (73.84%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int odds = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] % 2 != 0){
-                odds++;
-                if(odds == 3) return true;
-            }
-            else odds = 0;
+        int n = arr.length;
+        if(n < 3) return false;
+
+        for(int i=0; i<n-2; i++) {
+            // int p = arr[i] * arr[i+1] * arr[i+2];
+            int p = arr[i] & arr[i+1] & arr[i+2];
+            if(p % 2 == 1) return true;
         }
         return false;
     }
