01aaff10c739af089291d119ccb9fbff9a611659
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-03 14:44:59 +0530

    Time: 35 ms (7.35%) | Memory: 54.9 MB (97.11%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -3,16 +3,33 @@ class Solution {
         int n = nums.length;
         if(n <= 4) return 0;
 
-        Arrays.sort(nums);
-        int left = 0, right = n - 4;
-        int ans = Integer.MAX_VALUE;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
 
-        while(right < n) {
-            ans = Math.min(ans, nums[right] - nums[left]);
-            left++;
-            right++;
+
+        for(int num: nums) {
+            minHeap.add(num);
+            if(minHeap.size() > 4) minHeap.poll();
+
+            maxHeap.add(num);
+            if(maxHeap.size() > 4) maxHeap.poll();
+        }
+        //After this minHeap contains 4 largest numbers
+        //And maxHeap contains 4 smallest numbers
+
+        int[] fourLargest = new int[4];
+        int[] fourSmallest = new int[4];
+
+        for(int i=0; i<4; i++) {
+            fourLargest[i] = minHeap.poll();
+            fourSmallest[3 - i] = maxHeap.poll();
         }
 
-        return ans;
+        int maxDiff = Integer.MAX_VALUE;
+        for(int i=0; i<4; i++) {
+            maxDiff = Math.min(maxDiff, fourLargest[i] - fourSmallest[i]);
+        }
+
+        return maxDiff;
     }
 }
