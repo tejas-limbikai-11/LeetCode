bb839078fffd3194df299cd7b788e3d87845c70b
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-06 18:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (81.08%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,10 +1,43 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int pos = 0, neg = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] < 0) neg++;
-            else if(nums[i] > 0) pos++;
-        }
+        int pos = nums.length - firstPositive(nums);
+        int neg = lastNegative(nums) + 1;
         return Math.max(pos, neg);
+    }
+
+    public int firstPositive(int[] nums) {
+        int left = 0;
+        int right = nums.length-1; 
+        int ans = nums.length;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > 0) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int lastNegative(int[] nums) {
+        int left = 0;
+        int right = nums.length-1; 
+        int ans = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] < 0) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 }
