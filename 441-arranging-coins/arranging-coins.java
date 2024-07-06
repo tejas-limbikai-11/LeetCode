ed1aeed65502a43b521e0f9a1fca95b67c17c22c
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-06 18:13:44 +0530

    Time: 1 ms (97.11%) | Memory: 40.7 MB (70.18%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,10 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int i = 1;
-        while(n > 0) {
-            i++;
-            n -= i;
-        }
-        return i-1;
+        return (int) (Math.sqrt(2 * (long) n + 0.25) - 0.5);
     }
 }
