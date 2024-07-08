7f970773c1901064cdf4389862ed24e992b2c206
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-08 18:21:47 +0530

    Time: 3 ms (49.98%) | Memory: 40.6 MB (50.17%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
             list.add(i);
         }
         int index = 0;
@@ -9,6 +9,6 @@ class Solution {
             index = (index + k - 1) % list.size();
             list.remove(index);
         }
-        return list.get(0) + 1;
+        return list.get(0);
     }
 }
