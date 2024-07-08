a8ffa76f66e07c8e8dc54b6d998a8666891f1154
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-08 13:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (29.61%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,18 +1,34 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return sb(s).equals(sb(t));
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        while(i >= 0 || j >= 0) {
+            i = getNextValidCharIndex(s, i);
+            j = getNextValidCharIndex(t, j);
+
+            // reached the end of both strings hence s and t are equal
+            if(i < 0 && j < 0) return true;
+
+            // reached the end of one of the strings hence s and t cannot be equal
+            if(i < 0 || j < 0) return false;
+
+            if(s.charAt(i) != t.charAt(j)) return false;
+
+            i--;
+            j--;
+        }
+        return true;
     }
 
-    public String sb(String str) {
-        StringBuilder sb = new StringBuilder();
-
-        for(char ch: str.toCharArray()) {
-            if(ch != '#') 
-                sb.append(ch);
-
-            else if(sb.length() != 0) 
-                sb.deleteCharAt(sb.length()-1);
+    public int getNextValidCharIndex(String str, int end) {
+        int backspaceCount = 0;
+        while(end >= 0) {
+            if(str.charAt(end) == '#') backspaceCount++;
+            else if(backspaceCount > 0) backspaceCount--;
+            else break;
+            end--;
         }
-        return sb.toString();
+        return end;
     }
 }
