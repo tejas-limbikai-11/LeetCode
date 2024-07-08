23ccfbf73ac405767ddd1511db7e93654fc48612
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-08 12:44:25 +0530

    Time: 2 ms (86.63%) | Memory: 41.7 MB (32.00%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,27 +1,26 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer> s = new Stack<>();
+        List<Integer> list = new ArrayList<>();
 
         for(String str: operations) {
             if(str.equals("C")) {
-                s.pop();
+                list.remove(list.size()-1);
             } 
             else if(str.equals("D")) {
-                int score = s.peek();
-                s.push(score * 2);
+                int score = list.get(list.size()-1);
+                list.add(score * 2);
             }
-            else if(str.equals("+") && s.size() > 1) {
-                int prev1 = s.pop();
-                int prev2 = s.peek();
-                s.push(prev1);
-                s.push(prev1 + prev2);
+            else if(str.equals("+")) {
+                int prev1 = list.get(list.size()-1);
+                int prev2 = list.get(list.size()-2);
+                list.add(prev1 + prev2);
             }
-            else s.push(Integer.valueOf(str));
+            else list.add(Integer.valueOf(str));
         }
 
         int sum = 0;
-        while(!s.isEmpty()) {
-            sum += s.pop();
+        for(int i=0; i<list.size(); i++) {
+            sum += list.get(i);
         }
         return sum;
     }
