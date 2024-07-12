1ea479de3d8c16b96b96fe8a1cc3f5eb672075ff
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-12 22:59:27 +0530

    Time: 76 ms (66.67%) | Memory: 45.1 MB (96.97%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -7,25 +7,31 @@ class Solution {
         int maxScore = Math.max(x, y);
         int minScore = Math.min(x, y);
 
-        StringBuilder sb = new StringBuilder();
-        for(char ch: s.toCharArray()) {
-            sb.append(ch);
-            if(sb.length() >= 2 && sb.substring(sb.length()-2).equals(maxStr)) {
-                sb.setLength(sb.length()-2); //remove last 2 chars
-                score += maxScore;
-            }
-        }
+        String tempFirst = removeSubstring(s, maxStr);
+        int removedPairsCount = (s.length() - tempFirst.length()) / 2;
+        score += removedPairsCount * maxScore;
 
-        String newStr = sb.toString();
-        sb.setLength(0);
-        for(char ch: newStr.toCharArray()) {
-            sb.append(ch);
-            if(sb.length() >= 2 && sb.substring(sb.length()-2).equals(minStr)) {
-                sb.setLength(sb.length()-2); 
-                score += minScore;
-            }
-        }
+        String tempSecond = removeSubstring(tempFirst, minStr);
+        removedPairsCount = (tempFirst.length() - tempSecond.length())/2;
+        score += removedPairsCount * minScore;
 
         return score;
+    }
+
+    public String removeSubstring(String inputStr, String matchStr) {
+        StringBuilder sb = new StringBuilder();
+        int j = 0;
+
+        for(int i=0; i<inputStr.length(); i++) {
+            sb.append(inputStr.charAt(i));
+            j++;
+
+            if(j >= 2 && sb.charAt(j - 2) == matchStr.charAt(0) && sb.charAt(j - 1) == matchStr.charAt(1)) {
+                sb.delete(j-2, j);
+                j -= 2;
+            }
+        }
+
+        return sb.toString();
     }
 }
