bccd62a49d85c36ba72328b62c5ac35681ad09f6
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-19 20:38:41 +0530

    Time: 4 ms (24.97%) | Memory: 45.1 MB (51.10%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -3,32 +3,27 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        List<Integer> rowMin = new ArrayList<>();
+        int maxFromRowMin = Integer.MIN_VALUE;
         for(int row=0; row<m; row++) {
-            int min = Integer.MAX_VALUE;
+            int rowMin = Integer.MAX_VALUE;
             for(int col=0; col<n; col++) {
-                min = Math.min(min, matrix[row][col]);
+                rowMin = Math.min(rowMin, matrix[row][col]);
             }
-            rowMin.add(min);
+            maxFromRowMin = Math.max(maxFromRowMin, rowMin);
         }
 
-        List<Integer> colMax = new ArrayList<>();
+        int minFromColMax = Integer.MAX_VALUE;
         for(int col=0; col<n; col++) {
-            int max = Integer.MIN_VALUE;
+            int colMax = Integer.MIN_VALUE;
             for(int row=0; row<m; row++) {
-                max = Math.max(max, matrix[row][col]);
+                colMax = Math.max(colMax, matrix[row][col]);
             }
-            colMax.add(max);
+            minFromColMax = Math.min(minFromColMax, colMax);
         }
 
         List<Integer> result = new ArrayList<>();
-        for(int row=0; row<m; row++) {
-            for(int col=0; col<n; col++) {
-                if(matrix[row][col] == rowMin.get(row) 
-                && matrix[row][col] == colMax.get(col)) {
-                    result.add(matrix[row][col]);
-                }
-            }
+        if(maxFromRowMin == minFromColMax) {
+            result.add(maxFromRowMin);
         }
 
         return result;
