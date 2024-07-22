c11cae0f6937b8c5939c476685dfd2184ffd5deb
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-22 11:39:33 +0530

    Time: 10 ms (48.32%) | Memory: 45.4 MB (32.21%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,32 +1,17 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int height;
-        String name;
-
-        public Pair(int h, String n) {
-            this.height = h;
-            this.name = n;
-        }
-
-        @Override
-        public int compareTo(Pair other) {
-            return other.height - this.height; 
-        }
-    }
-
     public String[] sortPeople(String[] names, int[] heights) {
-        Pair[] people = new Pair[heights.length];
+        TreeMap<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
 
         for(int i=0; i<heights.length; i++) {
-            people[i] = new Pair(heights[i], names[i]);
+            map.put(heights[i], names[i]);
         }
-
-        Arrays.sort(people);
 
         String[] result = new String[heights.length];
 
-        for(int i=0; i<heights.length; i++) {
-            result[i] = people[i].name;
+        int i = 0;
+        for(int key: map.keySet()) {
+            result[i] = map.get(key);
+            i++;
         }
         return result;
     }
