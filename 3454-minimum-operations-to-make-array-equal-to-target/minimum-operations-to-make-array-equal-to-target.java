5e17668f94e76fa86085bb3307d43b0939d00233
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-23 23:52:34 +0530

    Time: 3 ms (78.81%) | Memory: 58.3 MB (71.80%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
@@ -1,18 +1,13 @@
 class Solution {
     public long minimumOperations(int[] nums, int[] target) {
         int n = nums.length;
-
-        int[] diff = new int[n];
-        for(int i=0; i<n; i++) {
-            diff[i] = target[i] - nums[i];
-        }
-
+        
         int prev  = 0;
         int curr = 0;
         long ans = 0;
 
         for(int i=0; i<n; i++) {
-            curr = diff[i];
+            curr = target[i] - nums[i];
 
             if((curr > 0 && prev < 0) || (curr < 0 && prev > 0)) {
                 ans += Math.abs(curr);
