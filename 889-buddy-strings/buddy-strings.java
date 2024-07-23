839dff669e6b7c7f12de9fd88d738c52273c8d47
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-23 13:41:54 +0530

    Time: 3 ms (62.67%) | Memory: 42.5 MB (35.89%) - LeetSync

--- 889-buddy-strings/buddy-strings.java
+++ 889-buddy-strings/buddy-strings.java
@@ -2,31 +2,30 @@ class Solution {
     public boolean buddyStrings(String s, String goal) {
         if(s.length() != goal.length()) return false;
         
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char ch: s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
         if(s.equals(goal)) {
-            for(char key: map.keySet()) {
-                if(map.get(key) > 1) return true;
+            int[] arr = new int[26];
+            for(char ch : s.toCharArray()) {
+                arr[ch - 'a']++;
+                if(arr[ch - 'a'] > 1) return true;
             }
             return false;
         }
 
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-
-        int count = 0;
+        List<Integer> indices = new ArrayList<>();
 
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) != goal.charAt(i)) {
-                set1.add(s.charAt(i));
-                set2.add(goal.charAt(i));
-                count++;
+                indices.add(i);
             }
         }
 
-        if(count == 2 && set1.equals(set2)) return true;
-        else return false;
+        if(indices.size() != 2) return false;
+
+        char sArray[] = s.toCharArray();
+        char temp = sArray[indices.get(0)];
+        sArray[indices.get(0)] = sArray[indices.get(1)];
+        sArray[indices.get(1)] = temp;
+
+        return new String(sArray).equals(goal);
     }
 }
