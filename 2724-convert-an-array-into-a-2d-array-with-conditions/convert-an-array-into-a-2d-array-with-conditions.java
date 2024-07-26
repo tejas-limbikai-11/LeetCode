b4d93260ea8b12bfed370f949a8f60a96843f108
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-26 12:22:09 +0530

    Time: 2 ms (90.18%) | Memory: 44.7 MB (61.68%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,28 +1,17 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
         List<List<Integer>> result = new ArrayList<>();
+        int freq[] = new int[nums.length + 1];
 
         for(int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            if(freq[num] >= result.size()) {
+                result.add(new ArrayList<>());
+            }
+            result.get(freq[num]).add(num);
+            
+            freq[num]++;
         }
 
-        while(!map.isEmpty()) {
-            List<Integer> list = new ArrayList<>();
-            List<Integer> keysToDelete = new ArrayList<>();
-
-            for(int key: map.keySet()) {
-                list.add(key);
-                map.put(key, map.getOrDefault(key, 0) - 1);
-                if(map.get(key) == 0) {
-                    keysToDelete.add(key);
-                }
-            }
-            for(int key: keysToDelete) {
-                map.remove(key);
-            }
-            result.add(list);
-        }
         return result;
     }
 }
