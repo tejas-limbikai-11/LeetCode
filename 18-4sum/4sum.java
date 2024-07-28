13cc96689f513f10a780c1040685cd02fffbcc66
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-29 02:11:29 +0530

    Time: 19 ms (45.82%) | Memory: 44.1 MB (43.13%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,31 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
         for(int i=0; i<n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
             for(int j=i+1; j<n; j++) {
-                Set<Long> hashSet = new HashSet<>();      
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1;
+                int l = n - 1;
 
-                for(int k=j+1; k<n; k++) {
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k];
-                    long fourth = target - sum;
+                while(k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
 
-                    if(hashSet.contains(fourth)) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add((int) fourth);
-                        Collections.sort(temp);
-                        set.add(temp);
+                    if(sum == target) {
+                        List<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        result.add(temp);
+                        k++;
+                        l--;
+
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
                     }
-                    hashSet.add((long) nums[k]);
+                    else if(sum < target) k++;
+                    else l--;
                 }
             }
         }
-        List<List<Integer>> result = new ArrayList<>(set);
+        
         return result;
     }
 }
