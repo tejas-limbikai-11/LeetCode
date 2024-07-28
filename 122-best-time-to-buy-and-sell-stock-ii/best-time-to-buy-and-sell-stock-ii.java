ea7371ba0d4463847b64aab8b861b87c67428308
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-28 10:40:28 +0530

    Time: 1 ms (90.21%) | Memory: 45.9 MB (21.42%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        List<Integer> list  = new ArrayList<>();
         int maxProfit = 0;
 
-        int x = prices[0];
         for(int i=0; i<prices.length - 1; i++) {
             if(prices[i+1] > prices[i]) {
                 maxProfit += (prices[i+1] - prices[i]);
