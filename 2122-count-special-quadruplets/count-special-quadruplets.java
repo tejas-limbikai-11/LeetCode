30c3a21dd07bc4878c82bb15c990754299fafc9c
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-29 20:58:58 +0530

    Time: 9 ms (92.50%) | Memory: 43 MB (9.09%) - LeetSync

--- 2122-count-special-quadruplets/count-special-quadruplets.java
+++ 2122-count-special-quadruplets/count-special-quadruplets.java
@@ -1,17 +1,17 @@
 class Solution {
     public int countQuadruplets(int[] nums) {
         int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
 
-        for(int i=0; i<n; i++) {
+        for(int i=n-2; i>=1; i--) {
             for(int j=i+1; j<n; j++) {
-                for(int k=j+1; k<n; k++) {
-                    for(int l=k+1; l<n; l++) {
-                        if(nums[i] + nums[j] + nums[k] == nums[l]) {
-                            count++;
-                        }
-                    }
-                }
+                int num = nums[j] - nums[i];
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            for(int j=0; j<i-1; j++) {
+                int num = nums[j] + nums[i-1];
+                count += map.getOrDefault(num, 0);
             }
         }
         return count;
