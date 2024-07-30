b57fc2a413c2d550b128195269b513aeff4c8975
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-30 23:17:50 +0530

    Time: 1 ms (94.09%) | Memory: 44.5 MB (42.13%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        solve(nums, temp, result, set);
+        solve(0, nums, result);
         return result;
     }
 
-    public void solve(int[] nums, List<Integer> temp, List<List<Integer>> result, Set<Integer> set) {
+    public void solve(int idx, int[] nums, List<List<Integer>> result) {
         int n = nums.length;
-        if(temp.size() == n) {
-            result.add(new ArrayList<>(temp));
+        if(idx == n) {
+            List<Integer> temp = new ArrayList<>();
+            for(int num: nums) {
+                temp.add(num);
+            }
+            result.add(temp);
             return;
         }
 
-        for(int i=0; i<n; i++) {
-            if(!set.contains(nums[i])) {
-                temp.add(nums[i]);
-                set.add(nums[i]);
-                solve(nums, temp, result, set);
-                set.remove(nums[i]);
-                temp.remove(temp.size() - 1);
-            }
+        for(int i = idx; i < n; i++) {
+            swap(nums, i, idx);
+            solve(idx + 1, nums, result);
+            swap(nums, i, idx);
         }
+    }
+
+    public void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
