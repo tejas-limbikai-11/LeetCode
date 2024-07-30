fde8d5bbbab5589923ca345f7efafa4a8fa887c2
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-31 01:12:56 +0530

    Time: 21 ms (84.82%) | Memory: 45.3 MB (77.24%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,22 +1,13 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-
-        int countA = 0;
-        for(int i=0; i<n; i++) {
-            if(s.charAt(i) == 'a') countA++;
-        }
-
-        int count = Integer.MAX_VALUE;
+        int dp = 0;     // count the extra 'a'
         int countB = 0;
 
-        for(int i=0; i<n; i++) {
-            if(s.charAt(i) == 'a') countA--;
-
-            count = Math.min(count, countA + countB);
-
+        for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == 'b') countB++;
+
+            else dp = Math.min(dp + 1, countB);     // 1. delete 'a' // 2. keep curr 'a', delete all 'b'         
         }
-        return count;
+        return dp;
     }
 }
