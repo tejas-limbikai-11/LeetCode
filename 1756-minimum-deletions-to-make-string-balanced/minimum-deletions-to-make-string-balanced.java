4ea91f76e472ced5ca138fabe79346049ee4e875
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-31 00:55:13 +0530

    Time: 38 ms (43.18%) | Memory: 45.7 MB (9.47%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,14 +1,7 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int[] leftB = new int[n];
         int[] rightA = new int[n];
-
-        int countB = 0;
-        for(int i=0; i<n; i++) {
-            leftB[i] = countB;
-            if(s.charAt(i) == 'b') countB++;
-        }
 
         int countA = 0;
         for(int i=n-1; i>=0; i--) {
@@ -16,9 +9,12 @@ class Solution {
             if(s.charAt(i) == 'a') countA++;
         }
 
+        int countB = 0;
         int count = Integer.MAX_VALUE;
         for(int i=0; i<n; i++) {
-            count = Math.min(count, leftB[i] + rightA[i]);
+            count = Math.min(count, countB + rightA[i]);
+            
+            if(s.charAt(i) == 'b') countB++;
         }
         return count;
     }
