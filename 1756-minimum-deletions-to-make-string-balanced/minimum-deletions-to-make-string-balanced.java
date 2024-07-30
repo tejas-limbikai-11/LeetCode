ab7ea3b824eb60eea3e9d0487703c22083841306
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-07-31 01:07:17 +0530

    Time: 43 ms (30.62%) | Memory: 45.4 MB (40.38%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,12 +1,22 @@
 class Solution {
     public int minimumDeletions(String s) {
+        int n = s.length();
+
         int countA = 0;
+        for(int i=0; i<n; i++) {
+            if(s.charAt(i) == 'a') countA++;
+        }
+
+        int count = Integer.MAX_VALUE;
         int countB = 0;
 
-        for(int i=0; i<s.length(); i++) {
+        for(int i=0; i<n; i++) {
+            if(s.charAt(i) == 'a') countA--;
+
+            count = Math.min(count, countA + countB);
+
             if(s.charAt(i) == 'b') countB++;
-            else if(countB > countA) countA++;
         }
-        return countA;
+        return count;
     }
 }
