ffc73a766b9e62f98669f0b79eb0bf26766afdfe
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-01 23:25:57 +0530

    Time: 1 ms (85.34%) | Memory: 42.8 MB (83.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,36 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generate(new StringBuilder(), n, 0, result);
+        generate(new StringBuilder(), n, 0, 0, result);
         return result;
     }
 
-    public boolean isValid(StringBuilder sb) {
-        int count = 0;
-        String str = sb.toString();
-        for(char ch: str.toCharArray()) {
-            if(ch == '(') count++;
-            else count--;
-
-            if(count < 0) return false;
-        }
-        return count == 0;
-    }
-
-    public void generate(StringBuilder curr, int n, int length, List<String> result) {
-        if(length == 2 * n) {
-            if(isValid(curr)) {
-                result.add(curr.toString());
-            }
+    public void generate(StringBuilder curr, int n, int open, int close, List<String> result) {
+        if(curr.length() == 2 * n) {
+            result.add(curr.toString());
             return;
         }
 
-        curr.append('(');
-        generate(curr, n, length + 1, result);
-        curr.deleteCharAt(curr.length() - 1);
+        if(open < n) {
+            curr.append('(');
+            generate(curr, n, open + 1, close, result);
+            curr.deleteCharAt(curr.length() - 1);
+        }
 
-        curr.append(')');
-        generate(curr, n, length + 1, result);
-        curr.deleteCharAt(curr.length() - 1);
+        if(open > close) {
+            curr.append(')');
+            generate(curr, n, open, close + 1, result);
+            curr.deleteCharAt(curr.length() - 1);
+        } 
     }
 }
