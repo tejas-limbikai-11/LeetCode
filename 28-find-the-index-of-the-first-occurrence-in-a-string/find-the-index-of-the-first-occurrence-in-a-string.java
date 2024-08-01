1781d9cbd78e0bd6fd77053339640ffc904b0e7f
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-01 19:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (91.64%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,43 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return search(haystack, needle);
-    }
+        int m = haystack.length();
+        int n = needle.length();
 
-    private final int PRIME = 101;
-
-    private double calculateHash(String str) {
-        double hash = 0;
-        for(int i=0; i < str.length(); i++) {
-        hash += str.charAt(i) * Math.pow(PRIME, i);
-        }
-        return hash;
-    }
-
-    private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
-        double newHash = (prevHash - oldChar) / PRIME;
-        newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
-        return newHash;
-    }
-
-    public int search(String text, String pattern) {
-        if(pattern.length() > text.length()) {
-            return -1;
-        }
-        int patternLength = pattern.length();
-        double patternHash = calculateHash(pattern);
-        double textHash = calculateHash(text.substring(0, patternLength));
-
-        for(int i=0; i<= text.length() - patternLength; i++) {
-        if(textHash == patternHash) {
-            if(text.substring(i, i+patternLength).equals(pattern)) {
-                return i;
+        for(int i=0; i<=m-n; i++) {
+            for(int j=0; j<n; j++) {
+                if(haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if(j == n-1) return i;
             }
         }
-
-        if (i < text.length() - patternLength) {
-            textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
-        }
-        }
         return -1;
-  }
+    }
 }
