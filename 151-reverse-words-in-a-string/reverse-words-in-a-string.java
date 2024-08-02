9dbfab3ea7a2de72609071f62b475d5ad62e9721
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-02 23:15:09 +0530

    Time: 29 ms (5.05%) | Memory: 42.2 MB (98.64%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,56 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split("\\s+");
+         if (s == null) return null;
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=words.length - 1; i>=0; i--) {
-            sb.append(words[i]).append(" ");
+        char[] str = s.toCharArray();
+        int start = 0, end = str.length - 1;
+
+        while(start <= end && str[start] == ' ') {
+            start++;
         }
 
-        return sb.toString().strip();
+        while(end >= 0 && str[end] == ' ') {
+            end--;
+        }
+
+        if(start > end) return "";
+
+        int i = start;
+
+        while(i <= end) {
+            if(str[i] != ' ') {
+                int j = i+1;
+                while(j <= end && str[j] != ' ') {
+                    j++;
+                }
+                reverse(str, i, j-1);
+                i = j;
+            }
+            else {
+                if(i > start && str[i - 1] == ' ') {
+                    int j = i;
+                    while(j <= end - 1) {
+                        str[j] = str[j+1];
+                        j++;
+                    }
+                    end--;
+                }
+                else i++;
+            }
+
+        }
+        reverse(str, start, end);
+
+        return new String(str, start, end - start + 1);
+    }
+
+    public void reverse(char[] str, int begin, int end) {
+        while (begin < end) {
+            char temp = str[begin];
+            str[begin] = str[end];
+            str[end] = temp;
+            begin++;
+            end--;
+        }
     }
 }
