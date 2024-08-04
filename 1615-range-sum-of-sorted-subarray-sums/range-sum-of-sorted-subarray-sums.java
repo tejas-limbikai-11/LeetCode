52b6d84046f2acfb5019724d23fe24f35d3ae81d
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-04 19:17:24 +0530

    Time: 85 ms (38.29%) | Memory: 44.6 MB (82.43%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,21 +1,27 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> subarraySum = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         int mod = 1_000_000_007;
 
         for(int i=0; i<n; i++) {
-            int sum = 0;
-            for(int j=i; j<n; j++) {
-                sum += nums[j];
-                subarraySum.add(sum);
-            }
+            pq.offer(new int[] {nums[i], i});
         }
 
-        Collections.sort(subarraySum);
-        int ans = 0;
-        for(int i=left-1; i<right; i++) {
-            ans = (ans + subarraySum.get(i)) % mod;
+        int result = 0;
+
+        for(int i=1; i<=right; i++) {
+            int[] curr = pq.poll();
+            int sum = curr[0];
+            int idx = curr[1];
+
+            if(i >= left) {
+                result = (result + sum) % mod;
+            }
+
+            if(curr[1] < n-1) {
+                pq.offer(new int[] {sum + nums[idx + 1], idx + 1});
+            }
         }
-        return ans;
+        return result;
     }
 }
