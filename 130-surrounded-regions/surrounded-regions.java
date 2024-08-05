30f960773f0984710ee987220de6f8be700bab56
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-05 20:28:13 +0530

    Time: 2 ms (86.98%) | Memory: 45.4 MB (43.87%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,39 +2,41 @@ class Solution {
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 // check for boundary 
                 if(i*j == 0 || i == m-1 || j == n-1) {
-                    dfs(board, i, j, visited);
+                    dfs(board, i, j);
                 }
             }
         }
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(!visited[i][j]) {
+                if(board[i][j] == 'O') {
                     board[i][j] = 'X';
+                }
+                else if(board[i][j] == 'Y') {
+                    board[i][j] = 'O';
                 }
             }
         }
     }
 
-    public void dfs(char[][] board, int i, int j, boolean[][] visited) {
+    public void dfs(char[][] board, int i, int j) {
         int m = board.length;
         int n = board[0].length;
 
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j] != 'O' || visited[i][j]) {
+        if(i<0 || i>=m || j<0 || j>=n || board[i][j] != 'O') {
             return;
         }
 
-        visited[i][j] = true;
+        board[i][j] = 'Y';
 
-        dfs(board, i+1, j, visited);
-        dfs(board, i-1, j, visited);
-        dfs(board, i, j+1, visited);
-        dfs(board, i, j-1, visited);
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
     }
 }
