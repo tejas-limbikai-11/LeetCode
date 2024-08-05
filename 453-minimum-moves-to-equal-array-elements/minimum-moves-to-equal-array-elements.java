0ed665e32ef98495af991cae877e53e11ccf1705
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-05 23:42:32 +0530

    Time: 2 ms (83.43%) | Memory: 45.9 MB (14.64%) - LeetSync

---
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+
+        for(int num : nums) {
+            min = Math.min(min, num);
+        }
+
+        int diff = 0;
+        for(int num: nums) {
+            if(num != min) {
+                diff += num - min;
+            }
+        }
+        return diff;
+    }
+}
