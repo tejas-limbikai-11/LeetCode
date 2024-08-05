fc92fdc707d4fcab52f88bb053ad12be6a580a72
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-05 23:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.55%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix, i, j, j, i);
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix, i, j, i, n - 1 - j);
+            }
+        }
+    }
+
+    private void swap(int[][] matrix, int i1, int j1, int i2, int j2) {
+        int temp = matrix[i1][j1];
+        matrix[i1][j1] = matrix[i2][j2];
+        matrix[i2][j2] = temp;
+    }
+}
