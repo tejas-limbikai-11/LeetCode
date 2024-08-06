5a8039961495d91ef4865a7fadb5ec97509075f4
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-06 14:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (65.63%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,24 +1,20 @@
 class Solution {
     public int minimumPushes(String word) {
-        Map<Character, Integer> map = new HashMap<>();
-        
-        int count = 1;
-        int charAssigned = 0;
-
-        for(char ch: word.toCharArray()) {
-            map.put(ch, count);
-            charAssigned++; 
-
-            if(charAssigned == 8) {
-                count++;
-                charAssigned = 0;
-            }
-        }
-
+        int n = word.length();
         int result = 0;
-        for(char ch: word.toCharArray()) {
-            result += map.get(ch);
+        int row = 1;
+
+        int quotient = n / 8;
+        int remainder = n % 8;
+
+        while(quotient > 0) {
+            result += 8 * row;
+            quotient--;
+            row++;
         }
+
+        result += remainder * row;
+        
         return result;
     }
 }
