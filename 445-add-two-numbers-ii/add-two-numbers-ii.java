1151adee0944eabdefa3fbb921a39d83db3347a1
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-12 20:08:02 +0530

    Time: 4 ms (18.52%) | Memory: 44.4 MB (55.70%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,42 +10,39 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head1 = reverseLL(l1);
-        ListNode head2 = reverseLL(l2);
         ListNode ans = new ListNode();
         int sum = 0;
         int carry = 0;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        ListNode head1 = l1, head2 = l2;
 
-        while(head1 != null || head2 != null) {
-            if(head1 != null) {
-                sum += head1.val;
-                head1 = head1.next;
+        while(head1 != null) {
+            s1.push(head1.val);
+            head1 = head1.next;
+        }
+
+        while(head2 != null) {
+            s2.push(head2.val);
+            head2 = head2.next;
+        }
+
+        while(!s1.isEmpty() || !s2.isEmpty()) {
+            if(!s1.isEmpty()) {
+                sum += s1.pop();
             }
-            if(head2 != null) {
-                sum += head2.val;
-                head2 = head2.next;
+            if(!s2.isEmpty()) {
+                sum += s2.pop();
             }
+
             ans.val = sum % 10;
             carry = sum / 10;
+            
             ListNode newNode = new ListNode(carry);
             newNode.next = ans;
             ans = newNode;
             sum = carry;
         }
         return (carry == 0) ? ans.next : ans;
-    }
-
-    public ListNode reverseLL(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode nextNode;
-
-        while(curr != null) {
-            nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        return prev;
     }
 }
