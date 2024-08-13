074f7ade46c40816761b27e991b5518125c52ea6
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-13 12:12:43 +0530

    Time: 4 ms (16.59%) | Memory: 48.4 MB (26.13%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,53 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public void reorderList(ListNode head) {
-        //find midnode
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        //reverse midnode to end
-        ListNode rev = reverseLL(slow);
-
+        Stack<ListNode> stack = new Stack<>();
         ListNode curr = head;
-
-        while(rev.next != null) {
-            ListNode tempCurr = curr.next;
-            curr.next = rev;
-
-            ListNode tempRev = rev.next;
-            rev.next = tempCurr;
-
-            curr = tempCurr;
-            rev = tempRev;
-        }
-        
-    }
-
-    public ListNode reverseLL(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next;
-
         while(curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            stack.push(curr);
+            curr = curr.next;
         }
-        return prev;
+
+        int k = stack.size()/2;
+        curr = head;
+
+        while(k-- > 0) {
+            ListNode topNode = stack.pop();
+            ListNode temp = curr.next;
+            curr.next = topNode;
+            topNode.next = temp;
+            curr = temp;
+        }
+        curr.next = null;
     }
 }
