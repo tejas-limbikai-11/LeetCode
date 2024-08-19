41aa091afeacbc5b77355cc0c27a10afc205c682
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-19 18:13:03 +0530

    Time: 103 ms (5.12%) | Memory: 56.5 MB (5.24%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,15 +1,21 @@
 class Solution {
     public int minSteps(int n) {
         if(n == 1) return 0;
-        return 1 + solve(1, 1, n);
+        int dp[][] = new int[1001][1001];
+        for(int[] d: dp) Arrays.fill(d, -1);
+        return 1 + solve(1, 1, n, dp);
     }
 
-    public int solve(int currA, int clipboard, int n) {
+    public int solve(int currA, int clipboard, int n, int[][] dp) {
         if(currA == n) return 0;
         if(currA > n) return 1000;
 
-        int copyAllpaste = 1 + 1 + solve(currA * 2, currA, n);
-        int paste = 1 + solve(currA + clipboard, clipboard, n);
+        if(dp[currA][clipboard] != -1) {
+            return dp[currA][clipboard];
+        }
+
+        int copyAllpaste = 1 + 1 + solve(currA * 2, currA, n, dp);
+        int paste = 1 + solve(currA + clipboard, clipboard, n, dp);
 
         return Math.min(copyAllpaste, paste);
     }
