05b9f45ab2edc8b57417e81e7c738b2ce4766924
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-20 19:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (10.52%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,19 +3,17 @@ class Solution {
         int n = nums.length;
         if(n == 1) return nums[0];
         int dp[] = new int[n+1];
-        dp[1] = nums[0];
 
-        for(int i=2; i<n; i++) {
-            int rob = nums[i-1] + dp[i-2];
+        for(int i=1; i<n; i++) {
+            int rob = nums[i-1] + ((i >= 2) ? dp[i-2] : 0);
             int skip = dp[i-1];
             dp[i] = Math.max(rob, skip);
         }
         int FirstHouseRobbed = dp[n-1];
 
         Arrays.fill(dp, 0);
-        dp[2] = nums[1];
 
-        for(int i=3; i<=n; i++) {
+        for(int i=2; i<=n; i++) {
             int rob = nums[i-1] + dp[i-2];
             int skip = dp[i-1];
             dp[i] = Math.max(rob, skip);
