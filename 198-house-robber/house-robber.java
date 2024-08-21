cd6958d40b21c453d48685b9b81f387591868750
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-21 09:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (22.16%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
 
-        dp[1] = nums[0];
+        int prevPrev = 0;
+        int prev = nums[0];
 
         for(int i=2; i<=n; i++) {
-            int rob = nums[i-1] + dp[i-2];
-            int skip = dp[i-1];
+            int rob = nums[i-1] + prevPrev;
+            int skip = prev;
 
-            dp[i] = Math.max(rob, skip);
+            int temp = Math.max(rob, skip);
+            prevPrev = prev;
+            prev = temp;
         }
         
-        return dp[n];
+        return prev;
     }
 }
