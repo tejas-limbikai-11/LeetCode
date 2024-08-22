18af3d2cff6e78fc7e943296487cc19af7029af8
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-22 17:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (51.69%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -2,9 +2,8 @@ class Solution {
     public int findComplement(int num) {
         int numBits = (int) (Math.log(num) / Math.log(2)) + 1;
 
-        for(int i=0; i<numBits; i++) {
-            num ^= (1 << i);
-        }
-        return num;
+        int bitMask = (1 << numBits) - 1;
+
+        return num ^ bitMask;
     }
 }
