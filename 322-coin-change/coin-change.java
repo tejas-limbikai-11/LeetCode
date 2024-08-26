ad4f1634571330e0583d0a4b583b697ada084603
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-26 20:46:18 +0530

    Time: 16 ms (60.72%) | Memory: 45.2 MB (21.57%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -25,21 +25,4 @@ class Solution {
         if(ans >= 1e9) return -1;
         return ans;
     }
-
-    public int solve(int idx, int target, int[] coins, int[][] dp) {
-        if(idx == 0) {
-            if(target  % coins[0] == 0) return target / coins[0];
-            else return (int) 1e9;
-        }
-
-        if(dp[idx][target] != -1) return dp[idx][target];
-
-        int skip = solve(idx - 1, target, coins, dp);
-
-        int take = Integer.MAX_VALUE;
-        if(coins[idx] <= target) {
-            take = 1 + solve(idx, target - coins[idx], coins, dp);
-        }
-        return dp[idx][target] = Math.min(take ,skip);
-    }
 }
