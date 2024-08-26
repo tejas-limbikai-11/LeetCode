4eb08649639ecc2ecd7acba04e17f6354cebbffd
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-26 20:39:47 +0530

    Time: 15 ms (78.35%) | Memory: 44.9 MB (29.70%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,9 +2,25 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
-        for(int[] d: dp) Arrays.fill(d, -1);
 
-        int ans = solve(n-1, amount, coins, dp);
+        for(int target=0; target<=amount; target++) {
+            if(target  % coins[0] == 0) dp[0][target] = target / coins[0];
+            else dp[0][target] = (int) 1e9;
+        }
+
+        for(int idx=1; idx<n; idx++) {
+            for(int target=0; target<=amount; target++) {
+                int skip = dp[idx - 1][target];
+
+                int take = Integer.MAX_VALUE;
+                if(coins[idx] <= target) {
+                    take = 1 + dp[idx][target - coins[idx]];
+                }
+                dp[idx][target] = Math.min(take ,skip);
+            }
+        }
+
+        int ans = dp[n-1][amount];
 
         if(ans >= 1e9) return -1;
         return ans;
