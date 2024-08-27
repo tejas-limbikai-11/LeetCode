6615f9b4f653a9b682acdf5565b3db2ef329a437
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-27 13:57:21 +0530

    Time: 8 ms (66.28%) | Memory: 44.4 MB (31.61%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,17 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(0, 0, target, nums);
+        int n = nums.length;
+        int totalSum = 0;
+        for(int num: nums) totalSum += num;
+
+        int[][] dp = new int[n][2 * totalSum + 1];
+        for(int[] d: dp) Arrays.fill(d, -1);
+        return solve(0, 0, target, nums, dp, totalSum);
     }
 
-    public int solve(int idx, int sum, int target, int[] nums) {
+    public int solve(int idx, int sum, int target, int[] nums, int[][] dp, int totalSum) {
         if(idx >= nums.length) {
             if(sum == target) return 1;
             else return 0;
         }
 
-        int negative = solve(idx + 1, sum - nums[idx], target, nums);
-        int positive = solve(idx + 1, sum + nums[idx], target, nums);
+        if(dp[idx][sum + totalSum] != -1) return dp[idx][sum + totalSum];
 
-        return negative + positive;
+        int negative = solve(idx + 1, sum - nums[idx], target, nums, dp, totalSum);
+        int positive = solve(idx + 1, sum + nums[idx], target, nums, dp, totalSum);
+
+        return dp[idx][sum + totalSum] = negative + positive;
     }
 }
