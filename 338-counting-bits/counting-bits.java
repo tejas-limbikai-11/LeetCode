be24f67cfbccce0f0a1cde4250a698a3aa79965a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-29 00:54:31 +0530

    Time: 2 ms (94.25%) | Memory: 46.4 MB (69.20%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,9 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
         int[] result = new int[n+1];
+        if(n == 0) return result;
+        result[0] = 0;
 
-        for(int i=0; i<=n; i++) {
-            result[i] = Integer.bitCount(i);
+        for(int i=1; i<=n; i++) {
+            if(i % 2 == 0) {                    //even
+                result[i] = result[i/2];
+            }                                   //odd
+            else result[i] = result[i/2] + 1;
         }
         return result;
     }
