2c28701c06530be96a91f004c960dae9adbb17a9
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-08-29 00:51:15 +0530

    Time: 2 ms (94.25%) | Memory: 46.5 MB (69.20%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+
+        for(int i=0; i<=n; i++) {
+            result[i] = Integer.bitCount(i);
+        }
+        return result;
+    }
+}
