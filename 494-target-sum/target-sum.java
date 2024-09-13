04173f76a4dd8ff82ec8d8c073368d1275ebee58
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-13 20:14:14 +0530

    Time: 4 ms (93.44%) | Memory: 42.9 MB (52.55%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -2,22 +2,35 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
         int totalSum = 0;
-        for(int num: nums) totalSum += num;
-
-        if (target > totalSum || target < -totalSum) return 0;
-
-        int[][] dp = new int[n + 1][2 * totalSum + 1];
-        dp[n][totalSum] = 1;
-
-        for(int idx=n-1; idx>=0; idx--) {
-            for(int sum=-totalSum; sum<=totalSum; sum++) {
-                int adjustedSum = sum + totalSum;
-                int negative = (adjustedSum - nums[idx] >= 0) ? dp[idx + 1][adjustedSum - nums[idx]] : 0;
-                int positive = (adjustedSum + nums[idx] < 2 * totalSum + 1) ? dp[idx + 1][adjustedSum + nums[idx]] : 0;
-
-                dp[idx][adjustedSum] = negative + positive;
-            }
+        for(int num: nums) {
+            totalSum += num;
         }
-        return dp[0][target + totalSum];
+
+        if((target + totalSum) % 2 != 0 || totalSum < target || totalSum + target < 0) {
+            return 0;
+        }
+        
+        int sum = (totalSum + target) / 2;
+
+        int[][] dp = new int[n + 1][sum + 1];
+        for(int[] d: dp) Arrays.fill(d, -1);
+
+        return solve(n, sum, nums, dp);
+    }
+
+    public int solve(int n, int sum, int[] nums, int[][] dp) {
+        if(n == 0 && sum == 0) return 1;
+        else if(n == 0) return 0;
+
+        if(dp[n][sum] != -1) {
+            return dp[n][sum];
+        }
+
+        if(nums[n-1] <= sum) {
+            int take = solve(n-1, sum - nums[n-1], nums, dp);
+            int notTake = solve(n-1, sum, nums, dp);
+            return dp[n][sum] = (take + notTake);
+        }
+        else return dp[n][sum] = solve(n-1, sum, nums, dp);
     }
 }
