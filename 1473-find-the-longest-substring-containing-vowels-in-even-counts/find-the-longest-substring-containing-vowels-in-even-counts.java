b981e6b14df58dd2d9acb246ab243583fc500d3c
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-15 20:58:07 +0530

    Time: 246 ms (9.25%) | Memory: 49 MB (27.76%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -8,19 +8,19 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
             if(ch == 'a') {
-                freq[0] ^= 1;
+                freq[0] = (freq[0] + 1) % 2;
             }
             else if(ch == 'e') {
-                freq[1] ^= 1;
+                freq[1] = (freq[1] + 1) % 2;
             }
             else if(ch == 'i') {
-                freq[2] ^= 1;
+                freq[2] = (freq[2] + 1) % 2;
             }
             else if(ch == 'o') {
-                freq[3] ^= 1;
+                freq[3] = (freq[3] + 1) % 2;
             }
             else if(ch == 'u') {
-                freq[4] ^= 1;
+                freq[4] = (freq[4] + 1) % 2;
             }
             StringBuilder sb = new StringBuilder();
             for(int f: freq) {
