6b4c984e8df9d8c2923abaebcf434d4ed962e1dd
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-18 10:35:54 +0530

    Time: 8 ms (31.74%) | Memory: 47.9 MB (29.06%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for(int[] d: dp) Arrays.fill(d, -1);
-        return solve(n, amount, coins, dp);
-    }
+        int[][] dp = new int[n+1][amount+1];
 
-    public static int solve(int n, int sum, int coins[], int[][] dp) {
-        if(n == 0 && sum == 0 || sum == 0) return 1;
-        else if(n == 0) return 0;
-
-        if(dp[n][sum] != -1) {
-            return dp[n][sum];
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<amount+1; j++) {
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0) dp[i][j] = 1;
+            }
         }
 
-        int take = 0;
-        if(coins[n-1] <= sum) {
-            take = solve(n, sum - coins[n-1], coins, dp);
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<amount+1; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
         }
-        int notTake = solve(n-1, sum, coins, dp);
-
-        return dp[n][sum] = take + notTake;
+        return dp[n][amount];
     }
 }
