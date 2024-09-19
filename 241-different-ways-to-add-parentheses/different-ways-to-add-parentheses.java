8b39c42e6281a10fb5491b06353e011eea50bf72
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-19 23:35:51 +0530

    Time: 2 ms (68.85%) | Memory: 42.8 MB (27.78%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,16 +1,19 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
+        int n = expression.length();
         List<Integer> result = new ArrayList<>();
-        return solve(expression);
+        return solve(expression, 0, n);
     }
 
-    public List<Integer> solve(String s) {
+    public List<Integer> solve(String s, int start, int end) {
         List<Integer> result = new ArrayList<>();
-        for(int i=0; i<s.length(); i++) {
+
+        for(int i=start; i<end; i++) {
             char ch = s.charAt(i);
+
             if(ch == '+' || ch == '-' || ch == '*') {
-                List<Integer> leftSubstring = solve(s.substring(0, i));
-                List<Integer> rightSubstring = solve(s.substring(i + 1));
+                List<Integer> leftSubstring = solve(s, start, i);
+                List<Integer> rightSubstring = solve(s, i+1, end);
 
                 for(int x: leftSubstring) {
                     for(int y: rightSubstring) {
@@ -28,9 +31,8 @@ class Solution {
             }
         }
         if(result.size() == 0) {
-            result.add(Integer.parseInt(s));
+            result.add(Integer.parseInt(s.substring(start, end)));
         }
         return result;
     }
-    
 }
