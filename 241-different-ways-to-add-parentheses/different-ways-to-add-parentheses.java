e0ff205cc01e6618c78d6328a8338d912e884db0
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-19 23:43:37 +0530

    Time: 5 ms (9.45%) | Memory: 41.8 MB (78.62%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -2,18 +2,22 @@ class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
         int n = expression.length();
         List<Integer> result = new ArrayList<>();
-        return solve(expression, 0, n);
+        Map<String, List<Integer>> map = new HashMap<>();
+        return solve(expression, 0, n, map);
     }
 
-    public List<Integer> solve(String s, int start, int end) {
+    public List<Integer> solve(String s, int start, int end, Map<String, List<Integer>> map) {
         List<Integer> result = new ArrayList<>();
+
+        String key = start + "," + end;
+        if(map.containsKey(key)) return map.get(key);
 
         for(int i=start; i<end; i++) {
             char ch = s.charAt(i);
 
             if(ch == '+' || ch == '-' || ch == '*') {
-                List<Integer> leftSubstring = solve(s, start, i);
-                List<Integer> rightSubstring = solve(s, i+1, end);
+                List<Integer> leftSubstring = solve(s, start, i, map);
+                List<Integer> rightSubstring = solve(s, i+1, end, map);
 
                 for(int x: leftSubstring) {
                     for(int y: rightSubstring) {
@@ -33,6 +37,7 @@ class Solution {
         if(result.size() == 0) {
             result.add(Integer.parseInt(s.substring(start, end)));
         }
+        map.put(key, result);
         return result;
     }
 }
