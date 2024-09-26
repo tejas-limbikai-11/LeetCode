0855f7df730e8e28e78ea6085b28e5c043b9c44e
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-09-26 12:47:10 +0530

    Time: 58 ms (63.34%) | Memory: 55.4 MB (76.49%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,24 +1,55 @@
 class Solution {
+    public class TrieNode {
+        TrieNode[] children = new TrieNode[10];
+    }
+
+    public TrieNode getNode() {
+        TrieNode newNode = new TrieNode();
+        
+        for(int i=0; i<10; i++) {
+            newNode.children[i] = null;
+        }
+        return newNode;
+    }
+
+    public void insert(TrieNode root, String word) {
+        TrieNode crawler = root;
+        for(char ch: word.toCharArray()) {
+            int idx = ch - '0';
+            if(crawler.children[idx] == null) {
+                crawler.children[idx] = getNode();
+            }
+            crawler = crawler.children[idx];
+        }
+    }
+
+    public int DFS(TrieNode root, String word) {
+        TrieNode crawler = root;
+        int count = 0;
+
+        for(char ch: word.toCharArray()) {
+            int idx = ch - '0';
+            if(crawler.children[idx] == null) {
+                return count;
+            }
+            count++;
+            crawler = crawler.children[idx];
+        }
+        return count;
+    }
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = getNode();
         int result = 0;
-        Set<Integer> set = new HashSet<>();
 
         for(int num: arr1) {
-            int val = num;
-            while(!set.contains(val) && val > 0) {
-                set.add(val);
-                val /= 10;
-            }
+            insert(root, String.valueOf(num));
         }
 
         for(int num: arr2) {
-            int val = num;
-
-            while(!set.contains(val) && val > 0) {
-                val /= 10;
-            }
-            result = Math.max(result, (int) Math.log10(val) + 1);
+            result = Math.max(result, DFS(root, String.valueOf(num)));
         }
+
         return result;
     }
 }
