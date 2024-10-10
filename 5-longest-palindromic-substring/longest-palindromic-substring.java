d5fc651661be436ceb2bf20f6f39f5612d312bee
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-10 19:37:50 +0530

    Time: 250 ms (26.43%) | Memory: 58.6 MB (5.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,10 +3,11 @@ class Solution {
         int n = s.length();
         int maxLength = 0;
         int start = 0;
+        Boolean[][] dp = new Boolean[n+1][n+1];
 
         for(int i=0; i<n; i++) {
             for(int j=i; j<n; j++) {
-                if(isPalindrome(s, i, j)) {
+                if(isPalindrome(s, i, j, dp)) {
                     if(j - i + 1 > maxLength) {
                         maxLength = j - i + 1;
                         start = i;
@@ -17,12 +18,16 @@ class Solution {
         return s.substring(start, start + maxLength);
     }
 
-    public boolean isPalindrome(String s, int i, int j) {
+    public boolean isPalindrome(String s, int i, int j, Boolean[][] dp) {
         if(i >= j) return true;
 
-        if(s.charAt(i) == s.charAt(j)) {
-            return isPalindrome(s, i + 1, j - 1);
+        if(dp[i][j] != null) {
+            return dp[i][j];
         }
-        else return false;
+
+        if(s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = isPalindrome(s, i + 1, j - 1, dp);
+        }
+        else return dp[i][j] = false;
     }
 }
