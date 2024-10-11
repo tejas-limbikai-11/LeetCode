6a257759cc66c545e6ce8192d4ec4e4f65673ef1
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-11 17:01:31 +0530

    Time: 3 ms (96.73%) | Memory: 45.2 MB (21.38%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        return editDistance(word1, word2);
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int[] d: dp) Arrays.fill(d, -1);
+        return editDistance(n, m, word1, word2, dp);
     }
 
-    public int editDistance(String str1, String str2) {
-        int n = str1.length();
-        int m = str2.length();
-        int dp[][] = new int[n+1][m+1];
+    public int editDistance(int i, int j, String s1, String s2, int[][] dp) {
+        if(i == 0) return j;
+        if(j == 0) return i;
 
-        for(int i=0; i<n+1; i++) {
-            for(int j=0; j<m+1; j++) {
-                if(i == 0) dp[i][j] = j;
-                if(j == 0) dp[i][j] = i;
-            }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
         }
 
-        for(int i=1; i<n+1; i++) {
-            for(int j=1; j<m+1; j++) {
-                if(str1.charAt(i-1) == str2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else {
-                    int add = dp[i][j-1] + 1;
-                    int delete = dp[i-1][j] + 1;
-                    int replace = dp[i-1][j-1] + 1;
-                    dp[i][j] = Math.min(add, Math.min(delete, replace));
-                }
-            }
+        if(s1.charAt(i-1) == s2.charAt(j-1)) {
+            return dp[i][j] = editDistance(i-1, j-1, s1, s2, dp);
         }
-        return dp[n][m];
+        else {
+            int add = 1 + editDistance(i, j-1, s1, s2, dp);
+            int delete = 1 + editDistance(i-1, j, s1, s2, dp);
+            int replace = 1 + editDistance(i-1, j-1, s1, s2, dp);
+
+            return dp[i][j] = Math.min(add, Math.min(delete, replace));
+        }
     }
 }
