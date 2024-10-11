1172d9899d3752e988a75f9122f20b175a0fb057
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-11 16:04:03 +0530

    Time: 22 ms (12.02%) | Memory: 51 MB (24.12%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,18 +4,18 @@ class Solution {
         int m = t.length();
         int[][] dp = new int[n+1][m+1];
         for(int[] d: dp) Arrays.fill(d, -1);
-        return solve(n-1, m-1, s, t, dp);
+        return solve(n, m, s, t, dp);
     }
 
     public int solve(int i, int j, String s, String t, int[][] dp) {
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
 
         if(dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        if(s.charAt(i) == t.charAt(j)) {
+        if(s.charAt(i-1) == t.charAt(j-1)) {
             int take = solve(i - 1, j - 1, s, t, dp);
             int skip = solve(i - 1, j, s, t, dp);
             return dp[i][j] = take + skip;
