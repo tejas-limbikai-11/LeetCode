34e1c21aefb0336bab3ca853953881000087da7d
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-11 17:18:20 +0530

    Time: 6 ms (15.14%) | Memory: 44.5 MB (95.20%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,28 +2,28 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int dp[][] = new int[n+1][m+1];
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
 
-        for(int i=0; i<n+1; i++) {
-            for(int j=0; j<m+1; j++) {
-                if(i == 0) dp[i][j] = j;
-                if(j == 0) dp[i][j] = i;
-            }
+        for(int j=0; j<m+1; j++) {
+            prev[j] = j;
         }
 
         for(int i=1; i<n+1; i++) {
+            curr[0] = i;
             for(int j=1; j<m+1; j++) {
                 if(word1.charAt(i-1) == word2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else {
-                    int add = dp[i][j-1] + 1;
-                    int delete = dp[i-1][j] + 1;
-                    int replace = dp[i-1][j-1] + 1;
-                    dp[i][j] = Math.min(add, Math.min(delete, replace));
+                    int add = curr[j-1] + 1;
+                    int delete = prev[j] + 1;
+                    int replace = prev[j-1] + 1;
+                    curr[j] = Math.min(add, Math.min(delete, replace));
                 }
             }
+            prev = curr.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
