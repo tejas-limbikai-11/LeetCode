23b83fd043ea7c1e40393f8bdcbe0b9ebe8dd1a7
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-13 22:58:48 +0530

    Time: 1 ms (100.00%) | Memory: 41.3 MB (93.89%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,24 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n+1][2*k+1];
+        int[] after = new int[2*k+1];
+        int[] curr = new int[2*k+1];
 
         for(int idx = n-1; idx >=0; idx--) {
             for(int transacNo = 2 * k - 1; transacNo >= 0; transacNo--) {
                 if(transacNo % 2 == 0) {
-                    int buy = dp[idx + 1][transacNo + 1] - prices[idx];
-                    int notBuy = dp[idx + 1][transacNo];
-                    dp[idx][transacNo] = Math.max(buy, notBuy);
+                    int buy = after[transacNo + 1] - prices[idx];
+                    int notBuy = after[transacNo];
+                    curr[transacNo] = Math.max(buy, notBuy);
                 }
                 else {
-                    int sell = dp[idx + 1][transacNo + 1] + prices[idx];
-                    int notSell = dp[idx + 1][transacNo];
-                    dp[idx][transacNo] = Math.max(sell, notSell);
+                    int sell = after[transacNo + 1] + prices[idx];
+                    int notSell = after[transacNo];
+                    curr[transacNo] = Math.max(sell, notSell);
                 }
             }
+            after = curr;
         }
-        return dp[0][0];
+        return after[0];
     }
 }
