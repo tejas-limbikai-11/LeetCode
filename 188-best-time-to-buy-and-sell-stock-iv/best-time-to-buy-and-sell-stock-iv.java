8e0debd13292e45cd0dbeb7a62385a5e11d7ed36
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-13 22:52:36 +0530

    Time: 2 ms (83.54%) | Memory: 43.3 MB (61.24%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -2,26 +2,21 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n+1][2*k+1];
-        for(int[] d: dp) Arrays.fill(d, -1);
-        return solve(0, 0, k, prices, dp);
-    }
 
-    public int solve(int idx, int transacNo, int k, int[] prices, int[][] dp) {
-        if(transacNo == 2 * k || idx == prices.length) return 0;
-
-        if(dp[idx][transacNo] != -1) {
-            return dp[idx][transacNo];
+        for(int idx = n-1; idx >=0; idx--) {
+            for(int transacNo = 2 * k - 1; transacNo >= 0; transacNo--) {
+                if(transacNo % 2 == 0) {
+                    int buy = dp[idx + 1][transacNo + 1] - prices[idx];
+                    int notBuy = dp[idx + 1][transacNo];
+                    dp[idx][transacNo] = Math.max(buy, notBuy);
+                }
+                else {
+                    int sell = dp[idx + 1][transacNo + 1] + prices[idx];
+                    int notSell = dp[idx + 1][transacNo];
+                    dp[idx][transacNo] = Math.max(sell, notSell);
+                }
+            }
         }
-
-        if(transacNo % 2 == 0) {
-            int buy = solve(idx + 1, transacNo + 1, k, prices, dp) - prices[idx];
-            int notBuy = solve(idx + 1, transacNo, k, prices, dp);
-            return dp[idx][transacNo] = Math.max(buy, notBuy);
-        }
-        else {
-            int sell = solve(idx + 1, transacNo + 1, k, prices ,dp) + prices[idx];
-            int notSell = solve(idx + 1, transacNo, k, prices, dp);
-            return dp[idx][transacNo] = Math.max(sell, notSell);
-        }
+        return dp[0][0];
     }
 }
