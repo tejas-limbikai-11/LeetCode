b2aa4548a5c202652b677120813becdbc0ef6a1f
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-13 19:19:03 +0530

    Time: 76 ms (36.76%) | Memory: 63.9 MB (38.68%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,30 +2,37 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][][] dp = new int[n+1][2][3];
-        for(int[][] d: dp) {
-            for(int[] d2: d) {
-                Arrays.fill(d2, -1);
+        
+        //if(cap == 0) return 0;
+        for(int idx = 0; idx < n; idx++) {
+            for(int canBuy = 0; canBuy <= 1; canBuy++) {
+                dp[idx][canBuy][0] = 0;
             }
         }
-        return solve(0, 1, 2, prices, dp);
-    }
 
-    public int solve(int idx, int canBuy, int cap, int[] prices, int[][][] dp) {
-        if(cap == 0 || idx == prices.length) return 0;
-
-        if(dp[idx][canBuy][cap] != -1) {
-            return dp[idx][canBuy][cap];
+        //if(idx == n) return 0;
+        for(int canBuy = 0; canBuy <= 1; canBuy++) {
+            for(int cap = 0; cap <= 2; cap++) {
+                dp[n][canBuy][cap] = 0;
+            }
         }
 
-        if(canBuy == 1) {
-            int buy = solve(idx + 1, 0, cap, prices, dp) - prices[idx];
-            int notBuy = solve(idx + 1, 1, cap, prices, dp);
-            return dp[idx][canBuy][cap] = Math.max(buy, notBuy);
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int canBuy = 0; canBuy <= 1; canBuy++) {
+                for(int cap = 1; cap <= 2; cap++) {
+                    if(canBuy == 1) {
+                        int buy = dp[idx + 1][0][cap] - prices[idx];
+                        int notBuy = dp[idx + 1][1][cap];
+                        dp[idx][canBuy][cap] = Math.max(buy, notBuy);
+                    }
+                    else {
+                        int sell = dp[idx + 1][1][cap - 1] + prices[idx];
+                        int notSell = dp[idx + 1][0][cap];
+                        dp[idx][canBuy][cap] = Math.max(sell, notSell);
+                    }
+                }
+            }
         }
-        else {
-            int sell = solve(idx + 1, 1, cap - 1, prices, dp) + prices[idx];
-            int notSell = solve(idx + 1, 0, cap, prices, dp);
-            return dp[idx][canBuy][cap] = Math.max(sell, notSell);
-        }
+        return dp[0][1][2];
     }
 }
