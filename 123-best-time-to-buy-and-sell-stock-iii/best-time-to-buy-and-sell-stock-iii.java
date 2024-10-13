a907276c3ec88e7a472475ec92fa8aced617ecfc
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-13 19:26:02 +0530

    Time: 7 ms (59.34%) | Memory: 60.7 MB (81.41%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,38 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n+1][2][3];
-        
-        //if(cap == 0) return 0;
-        for(int idx = 0; idx < n; idx++) {
-            for(int canBuy = 0; canBuy <= 1; canBuy++) {
-                dp[idx][canBuy][0] = 0;
-            }
-        }
-
-        //if(idx == n) return 0;
-        for(int canBuy = 0; canBuy <= 1; canBuy++) {
-            for(int cap = 0; cap <= 2; cap++) {
-                dp[n][canBuy][cap] = 0;
-            }
-        }
+        int[][] after = new int[2][3];
+        int[][] curr = new int[2][3];
 
         for(int idx = n-1; idx >= 0; idx--) {
             for(int canBuy = 0; canBuy <= 1; canBuy++) {
                 for(int cap = 1; cap <= 2; cap++) {
                     if(canBuy == 1) {
-                        int buy = dp[idx + 1][0][cap] - prices[idx];
-                        int notBuy = dp[idx + 1][1][cap];
-                        dp[idx][canBuy][cap] = Math.max(buy, notBuy);
+                        int buy = after[0][cap] - prices[idx];
+                        int notBuy = after[1][cap];
+                        curr[canBuy][cap] = Math.max(buy, notBuy);
                     }
                     else {
-                        int sell = dp[idx + 1][1][cap - 1] + prices[idx];
-                        int notSell = dp[idx + 1][0][cap];
-                        dp[idx][canBuy][cap] = Math.max(sell, notSell);
+                        int sell = after[1][cap - 1] + prices[idx];
+                        int notSell = after[0][cap];
+                        curr[canBuy][cap] = Math.max(sell, notSell);
                     }
                 }
             }
+            after = curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 }
