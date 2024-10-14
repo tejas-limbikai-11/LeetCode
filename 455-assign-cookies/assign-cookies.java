e031221f0c92512d972c5feab99cd6503b77d297
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-14 14:15:28 +0530

    Time: 10 ms (69.72%) | Memory: 49.8 MB (8.40%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
         Arrays.sort(g);
         Arrays.sort(s);
-        int count = 0;
 
-        int i = 0, j = 0;
+        return solve(0, 0, g, s);
+    }
 
-        while(i < g.length && j < s.length) {
-            if(g[i] <= s[j]) {
-                count++;
-                i++;
-                j++;
-            }
-            else j++;
+    public int solve(int i, int j, int[] g, int[] s) {
+        if(i == g.length || j == s.length) return 0;
+
+        if(g[i] <= s[j]) {
+            return 1 + solve(i + 1, j + 1, g, s);
         }
-        return count;
+        else return solve(i, j + 1, g, s);
     }
 }
