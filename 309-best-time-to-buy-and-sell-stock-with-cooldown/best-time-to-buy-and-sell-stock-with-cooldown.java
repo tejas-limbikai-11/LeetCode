600cee9d1471c47fa0eafc7779ed2a7c5c45d2db
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-14 11:37:03 +0530

    Time: 1 ms (84.78%) | Memory: 41.7 MB (46.80%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,26 +2,21 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        for(int[] d: dp) Arrays.fill(d, -1);
-        return solve(0, 1, prices, dp);
-    }
 
-    public int solve(int idx, int canBuy, int[] prices, int[][] dp) {
-        if(idx >= prices.length) return 0;
-
-        if(dp[idx][canBuy] != -1) {
-            return dp[idx][canBuy];
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int canBuy = 0; canBuy <= 1; canBuy++) {
+                if(canBuy == 1) {
+                    int buy = dp[idx + 1][0] - prices[idx];
+                    int notBuy = dp[idx + 1][1];
+                    dp[idx][canBuy] = Math.max(buy, notBuy);
+                }
+                else {
+                    int sell = (idx + 2 <= n) ? dp[idx + 2][1] + prices[idx] : prices[idx];
+                    int notSell = dp[idx + 1][0];
+                    dp[idx][canBuy] = Math.max(sell, notSell);
+                }
+            }
         }
-
-        if(canBuy == 1) {
-            int buy = solve(idx + 1, 0, prices, dp) - prices[idx];
-            int notBuy = solve(idx + 1, 1, prices, dp);
-            return dp[idx][canBuy] = Math.max(buy, notBuy);
-        }
-        else {
-            int sell = solve(idx + 2, 1, prices, dp) + prices[idx];
-            int notSell = solve(idx + 1, 0,prices, dp);
-            return dp[idx][canBuy] = Math.max(sell, notSell);
-        }
+        return dp[0][1];
     }
 }
