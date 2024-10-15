439b1e1296b0fb660cd5ff1df59280ec02358372
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-15 18:54:40 +0530

    Time: 618 ms (5.04%) | Memory: 77.2 MB (17.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,28 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n + 1][n + 1];
+        int[][] dp = new int[n+1][n+1];
         for(int[] d: dp) Arrays.fill(d, -1);
+        
         return solve(0, -1, nums, dp);
     }
 
     public int solve(int idx, int prevIdx, int[] nums, int[][] dp) {
-        if(idx >= nums.length) return 0;
+        if(idx == nums.length) return 0;
 
-        if(prevIdx != -1 && dp[idx][prevIdx] != -1) {
-            return dp[idx][prevIdx];
+        if(dp[idx][prevIdx + 1] != -1) {
+            return dp[idx][prevIdx + 1];
         }
 
-        int take = -1;
+        int take = 0;
         if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
             take = 1 + solve(idx + 1, idx, nums, dp);
         }
+
         int skip = solve(idx + 1, prevIdx, nums, dp);
 
-        if(prevIdx != -1) {
-            dp[idx][prevIdx] = Math.max(take, skip);
-        }
-
-        return Math.max(take, skip);
+        return dp[idx][prevIdx + 1] = Math.max(take, skip);
     }
 }
