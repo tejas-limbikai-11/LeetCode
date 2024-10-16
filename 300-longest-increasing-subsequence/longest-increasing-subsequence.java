cd3dd7778cdb4883c3bf0f39a8a7a7a218800570
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-16 18:28:57 +0530

    Time: 6 ms (89.25%) | Memory: 44.3 MB (47.83%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,38 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, 1);
-        int maxLIS = 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        int length = 1;
 
-        for(int idx = 0; idx < n; idx++) {
-            for(int prevIdx = 0; prevIdx < idx; prevIdx++) {
-                if(nums[prevIdx] < nums[idx]) {
-                    dp[idx] = Math.max(dp[idx], 1 + dp[prevIdx]);
-                }
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > list.get(list.size() - 1)) {
+                list.add(nums[i]);
+                length++;
             }
-            maxLIS = Math.max(maxLIS, dp[idx]); 
+            else {
+                // int idx = Collections.binarySearch(list, nums[i]);
+                // if(idx < 0) idx = Math.abs(idx) - 1;
+                
+                int idx = binarySearch(list, nums[i]);
+                list.set(idx, nums[i]);
+            }
         }
-        return maxLIS;
+        return length;
+    }
+
+    public int binarySearch(List<Integer> list, int key) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(list.get(mid) == key) return mid;
+            else if(list.get(mid) < key) {
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return high + 1;
     }
 }
