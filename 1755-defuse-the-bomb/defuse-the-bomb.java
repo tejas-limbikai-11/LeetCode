ef7b670d3343e5d022663b68266ec305a00bb671
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-11-19 13:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (86.21%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -2,26 +2,34 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
 
-       int[] result = new int[n];
+        int[] result = new int[n];
+        if(k == 0) return result;
 
-        for(int i=0; i<n; i++) {
-            int sum = 0;
-            int count = 0;
-            if(k > 0) {
-                for(int j=i+1; j<i+n && count < k; j++) {
-                    sum += code[j % n];
-                    count++;
-                }
-                result[i] = sum;
-            }
-            else if(k < 0) {
-                for(int j= i-1; j>i-n && count < Math.abs(k); j--) {
-                    sum += code[(j + n) % n];
-                    count++;
-                }
-                result[i] = sum;
-            }
-            else result[i] = 0;
+        int i = -1;
+        int j = -1;
+
+        if(k > 0) {
+            i = 1;
+            j = k;
+        }
+        else if(k < 0) {
+            i = n - Math.abs(k);
+            j = n - 1;
+        }
+
+        int windowSum = 0;
+        for(int x=i; x<=j; x++) {
+            windowSum += code[x];
+        }
+
+        for(int x=0; x<n; x++) {
+            result[x] = windowSum;
+
+            windowSum -= code[i % n];
+            i++;
+
+            windowSum += code[(j + 1) % n];
+            j++;
         }
 
         return result;
