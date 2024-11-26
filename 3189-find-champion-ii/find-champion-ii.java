0040678fb0837ed857b63f25a5f7b056b890fcaf
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-11-26 23:33:47 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (61.76%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] indegree = new int[n];
 
         for(int[] edge: edges) {
             int x = edge[0];
             int y = edge[1];
 
-            map.put(y, map.getOrDefault(y, 0) + 1);
+            indegree[y]++;
         }
 
         int count = 0;
         int result = 0;
 
         for(int i=0; i<n; i++) {
-            if(!map.containsKey(i)) {
+            if(indegree[i] == 0) {
                 result = i;
                 count++;
             }
