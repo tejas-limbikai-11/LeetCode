cc7f675b93c89d61bea81987a35054e9063520e0
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-11-29 01:39:44 +0530

    Time: 27 ms (45.01%) | Memory: 58.9 MB (47.16%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
+        int m = matrix.length;
         int n = matrix[0].length;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int[] row: matrix) {
+            StringBuilder rowPattern = new StringBuilder();
+            int firstVal = row[0];
+
+            for(int col = 0; col < n; col++) {
+                rowPattern.append(row[col] == firstVal ? "S" : "B");
+            }
+
+            map.put(rowPattern.toString(), map.getOrDefault(rowPattern.toString(), 0) + 1);
+        }
+
         int maxRows = 0;
-
-        for(int[] currRow: matrix) {
-            int count = 0;
-            int[] invertedRow = new int[n];
-
-            for(int col=0; col<n; col++) {
-                invertedRow[col] = currRow[col] == 0 ? 1 : 0;
-            }
-
-            for(int[] row: matrix) {
-                if(Arrays.equals(row, currRow) || Arrays.equals(row, invertedRow)) {
-                    count++;
-                }
-            }
-
+        for(int count: map.values()) {
             maxRows = Math.max(maxRows, count);
         }
 
