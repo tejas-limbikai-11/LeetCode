1a91f60890bb1f9ace397bc594125dc1909e29c4
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-06 23:43:14 +0530

    Time: 1 ms (90.80%) | Memory: 48.3 MB (6.26%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,39 +1,24 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-
-        for(int i=0; i<n; i++) {
-            adj.put(i, new ArrayList<>());
-        }
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-
         int count = 0;
         boolean[] visited = new boolean[n];
 
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
                 count++;
-                dfs(adj, i, visited);
+                dfs(isConnected, i, visited);
             }
         }
         return count;
     }
 
-    public void dfs(Map<Integer, List<Integer>> adj, int u, boolean[] visited) {
+    public void dfs(int[][] isConnected, int u, boolean[] visited) {
         visited[u] = true;
 
-        for(int v: adj.get(u)) {
-            if(!visited[v]) {
-                dfs(adj, v, visited);
+        for(int v = 0; v < isConnected.length; v++) {
+            if(!visited[v] && isConnected[u][v] == 1) {
+                dfs(isConnected, v, visited);
             }
         }
     }
