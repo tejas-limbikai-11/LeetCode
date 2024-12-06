2a9e4fa1ac0f04aa2534ebb2c20ca790a7675ec4
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-07 00:23:01 +0530

    Time: 1 ms (90.80%) | Memory: 47.5 MB (64.07%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,45 +1,29 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-
-        for(int i=0; i<n; i++) {
-            adj.put(i, new ArrayList<>());
-        }
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-
         int count = 0;
         boolean[] visited = new boolean[n];
 
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                bfs(adj, i, visited);
                 count++;
+                bfs(isConnected, i, visited);
             }
         }
         return count;
     }
 
-    public void bfs(Map<Integer, List<Integer>> adj, int u, boolean[] visited) {
+    public void bfs(int[][] isConnected, int u, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(u);
         visited[u] = true;
 
         while(!queue.isEmpty()) {
-            int curr = queue.poll();
+            u = queue.poll();
 
-            for(int v: adj.get(curr)) {
-                if(!visited[v]) {
-                    queue.offer(v);
-                    visited[v] = true;
+            for(int v = 0; v < isConnected.length; v++) {
+                if(!visited[v] && isConnected[u][v] == 1) {
+                    bfs(isConnected, v, visited);
                 }
             }
         }
