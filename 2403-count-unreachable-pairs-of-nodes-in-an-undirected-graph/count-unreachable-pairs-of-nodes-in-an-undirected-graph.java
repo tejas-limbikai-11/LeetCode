9eb6b846eeba08d3a64f0e5d729dc1b02b146e37
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-11 00:44:06 +0530

    Time: 9 ms (95.82%) | Memory: 106.1 MB (70.87%) - LeetSync

---
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,63 @@
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        long ans = 0;
+
+        int[] rank = new int[n];
+        int[] parent = new int[n];
+        for(int i=0; i<n; i++) {
+            parent[i] = i;
+        }
+
+        for(int[] edge: edges) {
+            int x = edge[0];
+            int y = edge[1];
+            int xParent = find(x, parent);
+            int yParent = find(y, parent);
+
+            if(xParent != yParent) {
+                union(x, y, parent, rank);
+            }
+        }
+
+        long[] componentNodes = new long[n];
+
+        for(int i=0; i<n; i++) {
+            int iParent = find(i, parent);
+            componentNodes[iParent]++;
+        }
+
+        int len = n;
+
+        for(int i=0; i<len; i++) {
+            long nodes = componentNodes[i];
+            ans = ans + (nodes * (n - nodes));
+            n -= nodes;
+        }
+
+        return ans;
+    }
+
+    public int find(int i, int[] parent) {
+        if(i == parent[i]) return i;
+
+        return parent[i] = find(parent[i], parent);
+    }
+
+    public void union(int x, int y, int[] parent, int[] rank) {
+        int xParent = find(x, parent);
+        int yParent = find(y, parent);
+
+        if(xParent == yParent) return;
+
+        if(rank[xParent] > rank[yParent]) {
+            parent[yParent] = xParent;
+        }
+        else if(rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        }
+        else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
+        }
+    }
+}
