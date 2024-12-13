9791f2de2bba9b641d22f15ae07282884c777fe1
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-14 00:14:33 +0530

    Time: 103 ms (97.55%) | Memory: 61.8 MB (16.67%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,17 +1,17 @@
 class Solution {
     public long findScore(int[] nums) {
         int n = nums.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
         long result = 0;
+        List<int[]> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            pq.offer(new int[]{nums[i], i});
+            list.add(new int[]{nums[i], i});
         }
+        Collections.sort(list, (a, b) -> (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
         boolean[] marked = new boolean[n];
 
-        while (!pq.isEmpty()) {
-            int curr[] = pq.poll();
-            int score = curr[0];
-            int idx = curr[1];
+        for(int i=0; i<n; i++) {
+            int score = list.get(i)[0];
+            int idx = list.get(i)[1];
 
             if (!marked[idx]) {
                 result += score;
