357cbedfb8ee2acb56e621658cb5210876faefa1
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-15 00:59:33 +0530

    Time: 64 ms (17.17%) | Memory: 61.8 MB (22.06%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.java
+++ 2868-continuous-subarrays/continuous-subarrays.java
@@ -1,20 +1,22 @@
-
 class Solution {
   public long continuousSubarrays(int[] nums) {
     int n = nums.length;
-    TreeMap<Integer, Integer> map = new TreeMap<>();
+    PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+    PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
 
     int i = 0;
     int j = 0;
     long count = 0;
+    
+    while(j < n) {
+      minHeap.offer(new int[] {nums[j], j});
+      maxHeap.offer(new int[] {nums[j], j});
 
-    while (j < n) {
-      map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
-      while (Math.abs(map.lastKey() - map.firstKey()) > 2) {
-        map.put(nums[i], map.get(nums[i]) - 1);
-        if (map.get(nums[i]) == 0) map.remove(nums[i]);
+      while(Math.abs(minHeap.peek()[0] - maxHeap.peek()[0]) > 2) {
         i++;
+
+        while(!maxHeap.isEmpty() && maxHeap.peek()[1] < i) maxHeap.poll();
+        while(!minHeap.isEmpty() && minHeap.peek()[1] < i) minHeap.poll();
       }
       count += j-i+1;
       j++;
