14f6bfcd7390eb56de0ca8c67085a3b9db7dc49a
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-12-20 00:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (34.84%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int result = 0;
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            max = Math.max(max, arr[i]);
+            if(max == i) result++;
+        }
+
+        return result;
+    }
+}
