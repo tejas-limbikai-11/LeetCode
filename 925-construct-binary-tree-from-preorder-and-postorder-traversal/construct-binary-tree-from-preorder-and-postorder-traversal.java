ed5c893b10ac2064554c42e17e2acc3bfc0a4041
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2025-02-23 23:21:50 +0530

    Time: 1 ms (53.87%) | Memory: 43.5 MB (53.87%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -1,10 +1,14 @@
 class Solution {
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
         int n = preorder.length;
-        return solve(0, 0, n-1, preorder, postorder);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            map.put(postorder[i], i);
+        }
+        return solve(0, 0, n-1, preorder, postorder, map);
     }
 
-    public TreeNode solve(int preStart, int postStart, int preEnd, int[] preorder, int[] postorder) {
+    public TreeNode solve(int preStart, int postStart, int preEnd, int[] preorder, int[] postorder, Map<Integer, Integer> map) {
         if(preStart > preEnd) return null;
 
         TreeNode root = new TreeNode(preorder[preStart]);
@@ -12,15 +16,12 @@ class Solution {
 
         int nextNode = preorder[preStart + 1];
 
-        int j = postStart;
-        while(postorder[j] != nextNode) {
-            j++;
-        }
+        int j = map.get(nextNode);
 
         int numNodes = j - postStart + 1;
 
-        root.left = solve(preStart + 1, postStart,  preStart + numNodes, preorder, postorder);
-        root.right = solve(preStart + numNodes + 1, j + 1,  preEnd, preorder, postorder);
+        root.left = solve(preStart + 1, postStart,  preStart + numNodes, preorder, postorder, map);
+        root.right = solve(preStart + numNodes + 1, j + 1,  preEnd, preorder, postorder, map);
 
         return root;
     }
