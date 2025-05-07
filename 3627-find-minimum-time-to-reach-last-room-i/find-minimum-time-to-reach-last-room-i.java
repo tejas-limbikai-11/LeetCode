eeac94e733bac59234cd79c3a9b8e09303e30cc3
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2025-05-08 03:00:24 +0530

    Time: 8 ms (68.36%) | Memory: 45.1 MB (51.94%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[] {0, 0, 0});
+
+        int[][] minTime = new int[n][m];
+        for(int[] min: minTime) {
+            Arrays.fill(min, Integer.MAX_VALUE);
+        }
+        minTime[0][0] = 0;
+
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int currTime = curr[0];
+            int x = curr[1];
+            int y = curr[2];
+
+            if(x == n-1 && y == m-1) {
+                return currTime;
+            }
+
+            for(int[] d: directions) {
+                int newX = x + d[0];
+                int newY = y + d[1];
+
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m) {
+                    int arrivalTime = currTime;
+                    if(arrivalTime < moveTime[newX][newY]) {
+                        arrivalTime = moveTime[newX][newY];
+                    }
+                    arrivalTime += 1;
+
+                    if(arrivalTime < minTime[newX][newY]) {
+                        minTime[newX][newY] = arrivalTime;
+                        pq.offer(new int[] {arrivalTime, newX, newY});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
