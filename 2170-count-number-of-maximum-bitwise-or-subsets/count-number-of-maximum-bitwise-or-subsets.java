699644bf3656ac253adb0d0cc423625ae992f5bb
Tejas Limbikai <118275996+tejas-limbikai-11@users.noreply.github.com>
2025-07-29 01:23:49 +0530

    Time: 6 ms (95.69%) | Memory: 41.2 MB (64.01%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,20 +1,22 @@
 public class Solution {
-    int count=0;
+    int count = 0;
+
     public int countMaxOrSubsets(int[] nums) {
         int max = 0;
-        for(int i:nums)
+        for (int i : nums)
             max = max | i;
-        backtrack(nums,0,max, 0);
+        backtrack(nums, 0, max, 0);
         return count;
     }
 
-    private void backtrack(int[] nums,int first,int max, int curr){
-        if(max == curr)
+    private void backtrack(int[] nums, int first, int max, int curr) {
+        if (max == curr)
             count++;
-        if(first>=nums.length) return;
+        if (first >= nums.length)
+            return;
 
-        for(int i = first;i<nums.length;i++){
-            backtrack(nums,i+1,max,curr|nums[i]);
+        for (int i = first; i < nums.length; i++) {
+            backtrack(nums, i + 1, max, curr | nums[i]);
         }
     }
 }
